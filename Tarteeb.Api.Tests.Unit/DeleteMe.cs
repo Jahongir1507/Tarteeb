e38bf30dd2cc0a47b1109ec1c421236a36e76eaa
Elbek <e.normurodov@mail.ru>
2022-11-13 07:55:20 -0800

    CODE RUB: Add Header File

--- Tarteeb.Api.Tests.Unit/DeleteMe.cs
+++ Tarteeb.Api.Tests.Unit/DeleteMe.cs
@@ -1,3 +1,8 @@
+//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//===============================
+
 using Xunit;
 
 namespace Tarteeb.Api.Tests.Unit

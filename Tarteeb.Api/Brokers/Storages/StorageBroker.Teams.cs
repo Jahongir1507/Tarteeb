341b066c154ac21a8bb037c04219b7c1b458b09e
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2022-11-17 20:06:42 +0300

    Brokers: Resolve Problem Again

--- Tarteeb.Api/Brokers/Storages/StorageBroker.Teams.cs
+++ Tarteeb.Api/Brokers/Storages/StorageBroker.Teams.cs
@@ -4,8 +4,6 @@
 //=================================
 
 using System;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;

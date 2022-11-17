7ec6359069c8e647c86536d74859b4ee73846659
Elbek <e.normurodov@mail.ru>
2022-11-17 08:26:42 -0800

    ShouldAddTicketAsync -> FAIL

---
+++ Tarteeb.Api/Services/Foundations/Tickets/TicketService.cs
@@ -0,0 +1,22 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System.Threading.Tasks;
+using Tarteeb.Api.Brokers.Storages;
+using Tarteeb.Api.Models.Tickets;
+
+namespace Tarteeb.Api.Services.Foundations.Tickets
+{
+    public class TicketService : ITicketService
+    {
+        private readonly IStorageBroker storageBroker;
+
+        public TicketService(IStorageBroker storageBroker) =>
+            this.storageBroker = storageBroker;
+
+        public ValueTask<Ticket> AddTicketAsync(Ticket ticket) =>
+           throw new System.NotImplementedException();
+    }
+}

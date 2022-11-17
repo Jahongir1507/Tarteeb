dd6006eaf245bd6cdbbf68d8c03e33e3be323932
masterdegreeineurope <masterdegreeineurope@gmail.com>
2022-11-17 18:17:26 +0500

    BROKERS: Delete User

--- Tarteeb.Api/Brokers/Storages/StorageBroker.Users.cs
+++ Tarteeb.Api/Brokers/Storages/StorageBroker.Users.cs
@@ -25,7 +25,7 @@ namespace Tarteeb.Api.Brokers.Storages
         public async ValueTask<User> UpdateUserAsync(User user) =>
             await UpdateAsync(user);
 
-        public async ValueTask<User> GetUserAsync(User user) =>
+        public async ValueTask<User> DeleteUserAsync(User user) =>
             await DeleteAsync(user);
     }
 }

19d589f66c66d06b963f799bed0f438727264e4e
Diyor <shumtakadiyor@gmail.com>
2022-11-19 15:21:45 +0500

    CODE RUB: Resolve conflicts

--- Tarteeb.Api/Brokers/Storages/StorageBroker.Teams.cs
+++ Tarteeb.Api/Brokers/Storages/StorageBroker.Teams.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Tarteeb.Api.Models.Teams;
+using Tarteeb.Api.Models;
 
 namespace Tarteeb.Api.Brokers.Storages
 {

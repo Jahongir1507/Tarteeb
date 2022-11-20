3a0e137fc126e8414f85de577e6f1fd88792b48f
Khusniddin <isroilovkhusni@gmail.com>
2022-11-21 00:40:51 +0500

    ShouldRetriveTicketByIdAsync -> FAIL

---
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Tickets/TicketServiceTests.cs
@@ -0,0 +1,48 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System;
+using Moq;
+using Tarteeb.Api.Brokers.Loggings;
+using Tarteeb.Api.Brokers.Storages;
+using Tarteeb.Api.Models.Tickets;
+using Tarteeb.Api.Services.Foundations.Tickets;
+using Tynamix.ObjectFiller;
+
+namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Tickets
+{
+    public partial class TicketServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly ITicketService ticketService;
+
+        public TicketServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.ticketService = new TicketService(
+                storageBroker: this.storageBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static Ticket CreateRandomTicket() =>
+            CreateTicketFiller(dates: GetRandomDateTimeOffset()).Create();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+             new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        public static Filler<Ticket> CreateTicketFiller(DateTimeOffset dates)
+        {
+            var filler = new Filler<Ticket>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dates);
+
+            return filler;
+        }
+    }
+}
---
+++ Tarteeb.Api/Services/Foundations/Tickets/TicketService.cs
@@ -0,0 +1,28 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System;
+using System.Threading.Tasks;
+using Tarteeb.Api.Brokers.Loggings;
+using Tarteeb.Api.Brokers.Storages;
+using Tarteeb.Api.Models.Tickets;
+
+namespace Tarteeb.Api.Services.Foundations.Tickets
+{
+    public class TicketService : ITicketService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public TicketService(IStorageBroker storageBroker, ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public async ValueTask<Ticket> RetrieveTicketByIdAsync(Guid ticketId) =>
+           throw new NotImplementedException(); 
+    }
+}

e32f7ee3d9682c33f7c485c35444bf1fa61f6dd3
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2022-11-20 18:11:50 +0100

    ShoulAddUserAsync -> FAIL

---
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.cs
@@ -0,0 +1,48 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//===============================
+
+using System;
+using Moq;
+using Tarteeb.Api.Brokers.Loggings;
+using Tarteeb.Api.Brokers.Storages;
+using Tarteeb.Api.Models;
+using Tarteeb.Api.Services.Foundations.Users;
+using Tynamix.ObjectFiller;
+
+namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Users
+{
+    public partial class UserServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IUserService userService;
+
+        public UserServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.userService = new UserService(
+                this.storageBrokerMock.Object,
+                this.loggingBrokerMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTime() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static User CreateRandomUser() =>
+            CreateUserFiller(GetRandomDateTime()).Create();
+
+        private static Filler<User> CreateUserFiller(DateTimeOffset dates)
+        {
+            var filler = new Filler<User>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dates);
+
+            return filler;
+        }
+    }
+}

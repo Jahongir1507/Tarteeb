6199775dcf7aa598ba2d4cc625a152ab69c66cd1
Elbek <e.normurodov@mail.ru>
2022-11-21 09:04:43 -0800

    ShouldThrowValidationExceptionOnAddIfTicketIsInvalidAndLogItAsync -> PASS

--- Tarteeb.Api/Services/Foundations/Tickets/TicketService.Validations.cs
+++ Tarteeb.Api/Services/Foundations/Tickets/TicketService.Validations.cs
@@ -3,19 +3,69 @@
 // Free to use to bring order in your workplace
 //=================================
 
-using Tarteeb.Api.Models.Tickets.Exceptions;
+using System;
 using Tarteeb.Api.Models.Tickets;
+using Tarteeb.Api.Models.Tickets.Exceptions;
 
 namespace Tarteeb.Api.Services.Foundations.Tickets
 {
     public partial class TicketService
     {
+        private static void ValidateTicket(Ticket ticket)
+        {
+            ValidateTicketNotNull(ticket);
+
+            Validate(
+                (Rule: IsInvalid(ticket.Id), Parameter: nameof(Ticket.Id)),
+                (Rule: IsInvalid(ticket.Title), Parameter: nameof(Ticket.Title)),
+                (Rule: IsInvalid(ticket.Deadline), Parameter: nameof(Ticket.Deadline)),
+                (Rule: IsInvalid(ticket.CreatedDate), Parameter: nameof(Ticket.CreatedDate)),
+                (Rule: IsInvalid(ticket.UpdatedDate), Parameter: nameof(Ticket.UpdatedDate)),
+                (Rule: IsInvalid(ticket.CreatedUserId), Parameter: nameof(Ticket.CreatedUserId)),
+                (Rule: IsInvalid(ticket.UpdatedUserId), Parameter: nameof(Ticket.UpdatedUserId)));
+        }
+
+        private static dynamic IsInvalid(Guid id) => new
+        {
+            Condition = id == default,
+            Message = "Id is required"
+        };
+
+        private static dynamic IsInvalid(string text) => new
+        {
+            Condition = string.IsNullOrWhiteSpace(text),
+            Message = "Text is required"
+        };
+
+        private static dynamic IsInvalid(DateTimeOffset date) => new
+        {
+            Condition = date == default,
+            Message = "Value is required"
+        };
+
         private static void ValidateTicketNotNull(Ticket ticket)
         {
             if (ticket is null)
             {
                 throw new NullTicketException();
             }
+        }
+
+        private static void Validate(params (dynamic Rule, string Parameter)[] validations)
+        {
+            var invalidTicketException = new InvalidTicketException();
+
+            foreach ((dynamic rule, string parameter) in validations)
+            {
+                if (rule.Condition)
+                {
+                    invalidTicketException.UpsertDataList(
+                        key: parameter,
+                        value: rule.Message);
+                }
+            }
+
+            invalidTicketException.ThrowIfContainsErrors();
         }
     }
 }

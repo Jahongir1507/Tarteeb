707a7306045e08c8acc6f067be3be280fe0f6ec9
Elbek <e.normurodov@mail.ru>
2022-11-21 08:45:03 -0800

    ShouldThrowValidationExceptionOnAddIfTicketIsInvalidAndLogItAsync -> FAIL

--- Tarteeb.Api.Tests.Unit/Services/Foundations/Tickets/TicketServiceTests.Validations.Add.cs
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Tickets/TicketServiceTests.Validations.Add.cs
@@ -45,5 +45,71 @@ namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Tickets
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("  ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfTicketIsInvalidAndLogItAsync(
+            string invalidString)
+        {
+            // given
+            var invalidTicket = new Ticket
+            {
+                Title = invalidString
+            };
+
+            var invalidTicketException = new InvalidTicketException();
+
+            invalidTicketException.AddData(
+                key: nameof(Ticket.Id),
+                values: "Id is required");
+
+            invalidTicketException.AddData(
+                key: nameof(Ticket.Title),
+                values: "Text is required");
+
+            invalidTicketException.AddData(
+                key: nameof(Ticket.Deadline),
+                values: "Value is required");
+
+            invalidTicketException.AddData(
+               key: nameof(Ticket.CreatedDate),
+               values: "Value is required");
+
+            invalidTicketException.AddData(
+               key: nameof(Ticket.UpdatedDate),
+               values: "Value is required");
+
+            invalidTicketException.AddData(
+               key: nameof(Ticket.CreatedUserId),
+               values: "Id is required");
+
+            invalidTicketException.AddData(
+              key: nameof(Ticket.UpdatedUserId),
+              values: "Id is required");
+
+            var expectedTicketValidationException = 
+                new TicketValidationException(invalidTicketException);
+
+            // when
+            ValueTask<Ticket> addTicketTask = this.ticketService.AddTicketAsync(invalidTicket);
+
+            TicketValidationException actualTicketValidationException =
+                await Assert.ThrowsAsync<TicketValidationException>(addTicketTask.AsTask);
+
+            // then
+            actualTicketValidationException.Should().BeEquivalentTo(expectedTicketValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedTicketValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertTicketAsync(It.IsAny<Ticket>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }

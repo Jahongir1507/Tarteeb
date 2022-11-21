fec048814988301c9a981130d90cd43ef9204c33
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2022-11-21 16:57:52 +0100

    ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync -> PASS

---
+++ Tarteeb.Api/Services/Foundations/Tickets/TicketService.Validaton.cs
@@ -0,0 +1,22 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using Tarteeb.Api.Models.Tickets.Exceptions;
+using Tarteeb.Api.Models.Tickets;
+
+namespace Tarteeb.Api.Services.Foundations.Tickets
+{
+    public partial class TicketService
+    {
+
+        private static void ValidateTicketNotNull(Ticket ticket)
+        {
+            if (ticket is null)
+            {
+                throw new NullTicketException();
+            }
+        }
+    }
+}

5bfc912ceba7ae3823fa93822992099da413ed8a
Diyor <shumtakadiyor@gmail.com>
2022-11-23 18:16:55 +0500

    ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync => WIP

---
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Validations.Add.cs
@@ -0,0 +1,23 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Users
+{
+    public partial class Class1
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync()
+        {
+
+        }
+    }
+}

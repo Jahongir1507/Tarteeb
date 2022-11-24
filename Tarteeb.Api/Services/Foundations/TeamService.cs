42800996893b11d0db3582446234ba261a1d38ac
iCloud <iCloud@WIN-V2SQOAAIU1T>
2022-11-24 14:23:54 +0500

    ShouldAddTeamAsync -> WIP

---
+++ Tarteeb.Api/Services/Foundations/TeamService.cs
@@ -0,0 +1,29 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System.Threading.Tasks;
+using Tarteeb.Api.Brokers.Loggings;
+using Tarteeb.Api.Brokers.Storages;
+using Tarteeb.Api.Models.Teams;
+
+namespace Tarteeb.Api.Services.Foundations
+{
+    public class TeamService : ITeamService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public TeamService(IStorageBroker storageBroker, ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<Team> AddTeamAsync(Team team)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

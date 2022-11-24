ae64ab16eba7669bc66cba089b9cc2899a1ceabb
Muhammad Muso <musogulomov123@gmail.com>
2022-11-24 16:43:47 +0500

    ShouldAddTeamAsync -> FAIL

---
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/TeamServiceTests.Logic.Add.cs
@@ -0,0 +1,45 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//===============================
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Tarteeb.Api.Models.Teams;
+using Xunit;
+
+namespace Tarteeb.Api.Tests.Unit.Services.Foundations
+{
+    public partial class TeamServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddTeamAsync()
+        {
+            //given
+            Team randomTeam = CreateRandomTeam();
+            Team inputTeam = randomTeam;
+            Team persistedTeam = inputTeam;
+            Team expectedTeam = persistedTeam.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+            broker.InsertTeamAsync(inputTeam))
+                .ReturnsAsync(persistedTeam);
+
+            //when
+            Team actuaTeam = await this.teamService
+                .AddTeamAsync(inputTeam);
+
+            //then
+            actuaTeam.Should().BeEquivalentTo(expectedTeam);
+
+            this.storageBrokerMock.Verify(broker =>
+            broker.InsertTeamAsync(inputTeam), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+
+        }
+    }
+}
---
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/TeamServiceTests.cs
@@ -0,0 +1,45 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//===============================
+
+using System;
+using Moq;
+using Tarteeb.Api.Brokers.Loggings;
+using Tarteeb.Api.Brokers.Storages;
+using Tarteeb.Api.Models.Teams;
+using Tarteeb.Api.Services.Foundations;
+using Tynamix.ObjectFiller;
+
+namespace Tarteeb.Api.Tests.Unit.Services.Foundations
+{
+    public partial class TeamServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly ITeamService teamService;
+
+        public TeamServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+            this.teamService = new TeamService(
+                this.storageBrokerMock.Object,
+                this.loggingBrokerMock.Object);
+        }
+        private static DateTimeOffset GetRandomDateTime() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static Team CreateRandomTeam() =>
+            CreateTeamFiller(GetRandomDateTime()).Create();
+        private static Filler<Team> CreateTeamFiller(DateTimeOffset dates)
+        {
+            var filler = new Filler<Team>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dates);
+
+            return filler;
+        }
+    }
+}

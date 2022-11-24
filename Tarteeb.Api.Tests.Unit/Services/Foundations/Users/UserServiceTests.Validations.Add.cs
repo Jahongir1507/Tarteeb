1c5ed19234f4d0983398c7bfa1b0cf83675937d4
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2022-11-24 14:35:05 +0300

    ShouldThrowValidationExceptionOnAddIfUserIsInvalidAndLogItAsync->FAIL

--- Tarteeb.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Validations.Add.cs
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Validations.Add.cs
@@ -5,15 +5,10 @@
 
 using FluentAssertions;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Tarteeb.Api.Models;
 using Tarteeb.Api.Models.Users.Exceptions;
 using Xunit;
-using Xunit.Sdk;
 
 namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Users
 {
@@ -25,7 +20,7 @@ namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Users
             //given
             User noUser = null;
             var nullUserException = new NullUserException();
-            var expectedUserValidationexception = 
+            var expectedUserValidationexception =
                 new UserValidationException(nullUserException);
             //when
             ValueTask<User> addUserTask =
@@ -34,11 +29,81 @@ namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Users
                 await Assert.ThrowsAsync<UserValidationException>(addUserTask.AsTask);
             //then
             actualUserValidationException.Should().BeEquivalentTo(
-               expectedUserValidationexception );
+               expectedUserValidationexception);
 
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogError(It.Is(SameExceptionAs(
                     expectedUserValidationexception))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+            broker.InsertUserAsync(It.IsAny<User>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfUserIsInvalidAndLogItAsync(
+            string invalidString)
+        {
+            //given
+            var invalidUser = new User
+            {
+                FirstName = invalidString
+            };
+            var invalidUserException = new InvalidUserException();
+
+            invalidUserException.AddData(
+                key: nameof(User.FirstName),
+                values: "Text is required");
+
+            invalidUserException.AddData(
+                key: nameof(User.LastName),
+                values: "Text is required");
+
+            invalidUserException.AddData(
+                key: nameof(User.Id),
+                values: "Id is required");
+
+            invalidUserException.AddData(
+                key: nameof(User.UpdatedDate),
+                values: "Value is required");
+
+            invalidUserException.AddData(
+                key: nameof(User.CreatedDate),
+                values: "Value is required");
+
+            invalidUserException.AddData(
+                key: nameof(User.BirthDate),
+                values: "Value is required");
+
+            invalidUserException.AddData(
+                key: nameof(User.ManagerId),
+                values: "Id is required");
+
+            invalidUserException.AddData(
+                key: nameof(User.Email),
+                values: "Text is required");
+
+            var expectedUserValidationException = new UserValidationException(
+                invalidUserException);
+
+            //when
+            ValueTask<User> addUserTask = this.userService.AddUserAsync(invalidUser);
+
+            UserValidationException actualUserValidationException =
+                await Assert.ThrowsAsync<UserValidationException>(addUserTask.AsTask);
+
+            //then
+            actualUserValidationException.Should().BeEquivalentTo(
+                expectedUserValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+            broker.LogError(It.Is(SameExceptionAs(
+                expectedUserValidationException))), Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
             broker.InsertUserAsync(It.IsAny<User>()), Times.Never);

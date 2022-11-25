a14c1912632f1c786dd97cbbe37dfec28f2a05fd
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2022-11-25 09:50:53 +0100

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ Tarteeb.Api/Models/Tickets/Exceptions/FailedTicketStorageException.cs
@@ -0,0 +1,17 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System;
+using Xeptions;
+
+namespace Tarteeb.Api.Models.Tickets.Exceptions
+{
+    public class FailedTicketStorageException : Xeption
+    {
+        public FailedTicketStorageException(Exception innerException)
+            : base(message: "Failed Ticket storage error occurred,contact support.", innerException)
+        { }
+    }
+}

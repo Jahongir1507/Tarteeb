f84c40ae9b184e34e0864f187ef870f6084df499
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2022-11-26 09:38:11 +0100

    ShouldThrowDependencyValidationExceptionOnAddIfDbConcurrencyErrorOccursAndLogItAsync -> FAIL

--- Tarteeb.Api.Tests.Unit/Services/Foundations/Tickets/TicketServiceTests.Exceptions.Add.cs
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Tickets/TicketServiceTests.Exceptions.Add.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using EFxceptions.Models.Exceptions;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using Tarteeb.Api.Models.Tickets;
 using Tarteeb.Api.Models.Tickets.Exceptions;
@@ -59,7 +60,7 @@ namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Tickets
             string someMessage = GetRandomString();
             var duplicateKeyException = new DuplicateKeyException(someMessage);
 
-            var alreadyExistsTicketException = 
+            var alreadyExistsTicketException =
                 new AlreadyExistsTicketException(duplicateKeyException);
 
             var expectedTicketDependencyValidationException =
@@ -83,6 +84,37 @@ namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Tickets
 
             this.loggingBrokerMock.Verify(broker => broker.LogError(It.Is(SameExceptionAs(
                 expectedTicketDependencyValidationException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationExceptionOnAddIfDbConcurrencyErrorOccursAndLogItAsync()
+        {
+            // given
+            Ticket someTicket = CreateRandomTicket();
+            var dbUpdateConcurrencyException = new DbUpdateConcurrencyException();
+
+            var lockedTicketException = new LockedTicketException(dbUpdateConcurrencyException);
+            var expectedTicketDependencyValidationException = new TicketDependencyValidationException(lockedTicketException);
+
+            this.storageBrokerMock.Setup(broker => broker.InsertTicketAsync(It.IsAny<Ticket>()))
+                .ThrowsAsync(dbUpdateConcurrencyException);
+
+            // when
+            ValueTask<Ticket> addTicketTask = this.ticketService.AddTicketAsync(someTicket);
+
+            TicketDependencyValidationException actualTicketDependencyValidationException =
+                 await Assert.ThrowsAsync<TicketDependencyValidationException>(addTicketTask.AsTask);
+
+            // then
+            actualTicketDependencyValidationException.Should().BeEquivalentTo(expectedTicketDependencyValidationException);
+
+            this.storageBrokerMock.Verify(broker => broker.InsertTicketAsync(It.IsAny<Ticket>()), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker => broker.LogError(It.Is(
+                SameExceptionAs(expectedTicketDependencyValidationException))), Times.Once);
 
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();

75eef932d1ef8ddbfc7f275442795f5f0f34402c
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2022-11-28 19:20:19 +0300

    CODE RUB: Change Version yml File

--- Tarteeb.Api.Infrastructure.Build/Program.cs
+++ Tarteeb.Api.Infrastructure.Build/Program.cs
@@ -44,7 +44,7 @@ var githubPipeline = new GithubPipeline
 
                       TargetDotNetVersion = new TargetDotNetVersion
                       {
-                          DotNetVersion = "7.0.0",
+                          DotNetVersion = "7.0.100",
                           IncludePrerelease = true
                       }
                   },

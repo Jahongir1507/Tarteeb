c9975bb4cfbadd53f6060ebab3ed854edf2f16d8
Diyor <shumtakadiyor@gmail.com>
2022-12-06 20:52:38 +0500

    ShouldThrowValidationExceptionOnAddIfCreatedDateIsNotRecentAndLogItAsync->FAIL

--- Tarteeb.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.cs
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.cs
@@ -15,6 +15,7 @@ using Tarteeb.Api.Models;
 using Tarteeb.Api.Services.Foundations.Users;
 using Tynamix.ObjectFiller;
 using Xeptions;
+using Xunit;
 
 namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Users
 {
@@ -36,23 +37,44 @@ namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Users
                dateTimeBroker: this.dateTimeBrokerMock.Object,
                loggingBroker: this.loggingBrokerMock.Object);
         }
+       public static TheoryData<int>InvalidSeconds()
+        {
+            int secondsInPast = -1 * new IntRange(
+                min: 60,
+                max: short.MaxValue).GetValue();
+
+            int secondsInFuture=new IntRange(
+                min:0,
+                max: short.MaxValue).GetValue();
+
+            return new TheoryData<int>
+            {
+                secondsInPast,
+                secondsInFuture
+            };
+        }
+        
         private Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
            actualException => actualException.SameExceptionAs(expectedException);
 
         private static SqlException CreateSqlException() =>
             (SqlException)FormatterServices.GetUninitializedObject(typeof(SqlException));
+        
         private static string GetRandomString() =>
             new MnemonicString().GetValue();
+        
         private static DateTimeOffset GetRandomDateTime() =>
             new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
 
         private static User CreateRandomUser() =>
-            CreateUserFiller().Create();
+            CreateUserFiller(GetRandomDateTime()).Create();
+      
+        private static User CreateRandomUser(DateTimeOffset dates) =>
+            CreateUserFiller(dates).Create();
 
-        private static Filler<User> CreateUserFiller()
+        private static Filler<User> CreateUserFiller(DateTimeOffset dates)
         {
-            var filler = new Filler<User>();
-            DateTimeOffset dates = GetRandomDateTime();
+            var filler = new Filler<User>();         
 
             filler.Setup()
                 .OnType<DateTimeOffset>().Use(dates);

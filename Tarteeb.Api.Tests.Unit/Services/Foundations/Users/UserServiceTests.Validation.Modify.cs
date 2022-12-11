6ab35c7e750c0239ab3518739fb660406393d754
Diyor <shumtakadiyor@gmail.com>
2022-12-11 15:54:41 +0500

    ShouldThrowValidationExceptionOnModifyIfUserIsInvalidAndLogItAsync->FAIL

--- Tarteeb.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Validation.Modify.cs
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Validation.Modify.cs
@@ -12,6 +12,7 @@ using Tarteeb.Api.Models.Users.Exceptions;
 using Tarteeb.Api.Models;
 using Xunit;
 using Moq;
+using FluentAssertions;
 
 namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Users
 {
@@ -44,6 +45,72 @@ namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Users
 
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfUserIsInvalidAndLogItAsync(string invalidText)
+        {
+            //given
+            var invalidUser = new User
+            {
+                FirstName = invalidText
+            };
+
+            var invalidUserException=new InvalidUserException();
+
+            invalidUserException.AddData(
+                key: nameof(User.Id),
+                values: "Id is required");
+
+            invalidUserException.AddData(
+                key: nameof(User.FirstName),
+                values: "Text is required");
+
+            invalidUserException.AddData(
+                key: nameof(User.LastName),
+                values: "Text is required");
+
+            invalidUserException.AddData(
+                key: nameof(User.Email),
+                values: "Text is required");
+
+            invalidUserException.AddData(
+                key: nameof(User.BirthDate),
+                values: "Value is required");
+
+            invalidUserException.AddData(
+                key: nameof(User.CreatedDate),
+                values: "Value is required");
+
+            invalidUserException.AddData(
+                key: nameof(User.UpdatedDate),
+                values: "Value is required");
+
+            var expectedUserValidationException = new UserValidationException(
+                invalidUserException);
+               
+            //when
+            ValueTask<User> modifyUserTask = this.userService.ModifyUserAsync(invalidUser);
+
+            UserValidationException actualUserValidationException =
+                await Assert.ThrowsAsync<UserValidationException>(modifyUserTask.AsTask);
+
+            //then
+            actualUserValidationException.Should().BeEquivalentTo(
+                expectedUserValidationException);
+
+            this.loggingBrokerMock.Verify(broker=>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedUserValidationException))),Times.Once);
+
+            this.storageBrokerMock.Verify(broker=>
+                broker.InsertUserAsync(It.IsAny<User>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
     }

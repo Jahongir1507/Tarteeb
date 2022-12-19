f0ffe3f058fc74bd1bfd34898219dca06f2b82d7
Muhammad Muso <musogulomov123@gmail.com>
2022-12-19 20:54:23 +0500

    EXPOUSERS: Post Team WIP

---
+++ Tarteeb.Api/Controllers/TeamsController.cs
@@ -0,0 +1,24 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using Tarteeb.Api.Models.Teams;
+
+namespace Tarteeb.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TeamsController : RESTFulController
+    {
+        [HttpPost]
+        public async ValueTask<ActionResult<Team>> PostTeamAsync(Team team)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

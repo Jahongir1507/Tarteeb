8a7c884fdec36cc103d6f3dbcf42512d809107ef
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-20 10:40:29 -0800

    ShouldThrowNotFoundExceptionOnRetrieveByIdIfTeamIsNotFoundAndLogItAsync -> FAIL

--- Tarteeb.Api.Tests.Unit/Services/Foundations/Teams/TeamServiceTests.Validations.RetrieveById.cs
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Teams/TeamServiceTests.Validations.RetrieveById.cs
@@ -6,6 +6,7 @@
 using System;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Microsoft.Extensions.Hosting;
 using Moq;
 using Tarteeb.Api.Models.Teams;
 using Tarteeb.Api.Models.Teams.Exceptions;
@@ -49,6 +50,46 @@ namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Teams
 
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowNotFoundExceptionOnRetrieveByIdIfTeamIsNotFoundAndLogItAsync()
+        {
+            //given
+            Guid someTeamId = Guid.NewGuid();
+            Team noTeam = null;
+
+            var notFoundTeamException =
+                new NotFoundTeamException(someTeamId);
+
+            var expectedTeamValidationException =
+                new TeamValidationException(notFoundTeamException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectTeamByIdAsync(It.IsAny<Guid>()))
+                    .ReturnsAsync(noTeam);
+
+            //when
+            ValueTask<Team> retrieveTeamByIdTask =
+                this.teamService.RetrieveTeamByIdAsync(someTeamId);
+
+            TeamValidationException actualTeamValidationException =
+                await Assert.ThrowsAsync<TeamValidationException>(
+                    retrieveTeamByIdTask.AsTask);
+
+            // then
+            actualTeamValidationException.Should().BeEquivalentTo(expectedTeamValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectTeamByIdAsync(It.IsAny<Guid>()), Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedTeamValidationException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
     }

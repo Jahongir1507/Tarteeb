bba5099657c8549e6f8f6c9a153b5ed89eefcf70
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-20 08:54:44 -0800

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync -> FAIL

---
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Teams/TeamServiceTests.Validations.RetrievById.cs
@@ -0,0 +1,56 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Tarteeb.Api.Models.Teams;
+using Tarteeb.Api.Models.Teams.Exceptions;
+using Xunit;
+
+namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Teams
+{
+    internal partial class TeamServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            var invalidTeamId = Guid.Empty;
+
+            var invalidTeamException = new InvalidTeamException();
+
+            invalidTeamException.AddData(
+                key: nameof(Team.Id),
+                values: "Id is required");
+
+            var expectedTeamValidationException = new
+                TeamValidationException(invalidTeamException);
+
+            // when
+            ValueTask<Team> retrieveTeamByIdTask =
+                this.teamService.RetrieveTeamByIdAsync(invalidTeamId);
+
+            TeamValidationException actualTeamValidationException =
+                await Assert.ThrowsAsync<TeamValidationException>(
+                    retrieveTeamByIdTask.AsTask);
+
+            // then
+            actualTeamValidationException.Should().BeEquivalentTo(expectedTeamValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedTeamValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectTeamByIdAsync(It.IsAny<Guid>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

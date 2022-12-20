c7f6417e5f04974666d2682f2d64fc1e99835023
masterdegreeineurope <masterdegreeineurope@gmail.com>
2022-12-20 23:29:45 +0500

    EXPOSERS: Get All Users

---
+++ Tarteeb.Api/Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using System.Linq;
+using Tarteeb.Api.Models;
+using Tarteeb.Api.Models.Users.Exceptions;
+using Tarteeb.Api.Services.Foundations.Users;
+
+namespace Tarteeb.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : RESTFulController
+    {
+        private readonly IUserService userService;
+
+        public UsersController(IUserService userService) =>
+            this.userService = userService;
+
+        [HttpGet]
+        public ActionResult<IQueryable<User>> GetAllUsers()
+        {
+            try
+            {
+                IQueryable<User> allUsers = this.userService.RetrieveAllUsers();
+
+                return Ok(allUsers);
+            }
+            catch (UserDependencyException userDependencyException)
+            {
+                return InternalServerError(userDependencyException.InnerException);
+            }
+            catch (UserServiceException userServiceException)
+            {
+                return InternalServerError(userServiceException.InnerException);
+            }
+        }
+    }
+}

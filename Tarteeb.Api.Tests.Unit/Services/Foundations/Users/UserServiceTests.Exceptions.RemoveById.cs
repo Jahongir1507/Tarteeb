9398069f3708dc52c9f573a7d9f50c9351448e52
GulchexrasGithub <burgulchexra@gmail.com>
2022-12-21 22:06:21 +0500

    ShouldThrowDependencyExceptionOnDeleteWhenSqlExceptionOccursAndLogItAsync -> PASS

---
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Exceptions.RemoveById.cs
@@ -0,0 +1,55 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Tarteeb.Api.Models;
+using Tarteeb.Api.Models.Users.Exceptions;
+using Xunit;
+
+namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Users
+{
+    public partial class UserServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnDeleteWhenSqlExceptionOccursAndLogItAsync()
+        {
+            //given 
+            Guid someUserId = Guid.NewGuid();
+            SqlException sqlException = CreateSqlException();
+
+            var failedUserStorageException =
+                new FailedUserStorageException(sqlException);
+
+            var expectedUserDependencyException =
+                new UserDependencyException(failedUserStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectUserByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(sqlException);
+
+            //when
+            ValueTask<User> deleteUserTask =
+                this.userService.RemoveUserByIdAsync(someUserId);
+
+            //then
+            await Assert.ThrowsAsync<UserDependencyException>(() =>
+                deleteUserTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectUserByIdAsync(It.IsAny<Guid>()), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedUserDependencyException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

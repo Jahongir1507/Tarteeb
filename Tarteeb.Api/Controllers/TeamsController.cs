ad7986e2d2ff430bafa7e9095523ee158319d5e2
Muhammad Muso <musogulomov123@gmail.com>
2022-12-21 12:17:34 +0500

    EXPOSERS: Post Team

--- Tarteeb.Api/Controllers/TeamsController.cs
+++ Tarteeb.Api/Controllers/TeamsController.cs
@@ -3,11 +3,12 @@
 // Free to use to bring order in your workplace
 //=================================
 
-using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RESTFulSense.Controllers;
 using Tarteeb.Api.Models.Teams;
+using Tarteeb.Api.Models.Teams.Exceptions;
+using Tarteeb.Api.Services.Foundations.Teams;
 
 namespace Tarteeb.Api.Controllers
 {
@@ -15,10 +16,39 @@ namespace Tarteeb.Api.Controllers
     [Route("api/[controller]")]
     public class TeamsController : RESTFulController
     {
+        private readonly ITeamService teamService;
+
+        public TeamsController(ITeamService teamService) =>
+            this.teamService = teamService;
+
         [HttpPost]
         public async ValueTask<ActionResult<Team>> PostTeamAsync(Team team)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return await this.teamService.AddTeamAsync(team);
+            }
+            catch (TeamValidationException teamValidationExpection)
+            {
+                return BadRequest(teamValidationExpection.InnerException);
+            }
+            catch (TeamDependencyValidationException teamDependencyValidationException)
+                when (teamDependencyValidationException.InnerException is AlreadyExistsTeamException)
+            {
+                return Conflict(teamDependencyValidationException.InnerException);
+            }
+            catch (TeamDependencyValidationException teamDependencyValidationException)
+            {
+                return BadRequest(teamDependencyValidationException.InnerException);
+            }
+            catch (TeamDependencyException teamDependencyException)
+            {
+                return InternalServerError(teamDependencyException.InnerException);
+            }
+            catch (TeamServiceException teamServiceException)
+            {
+                return InternalServerError(teamServiceException.InnerException);
+            }
         }
     }
 }
--- Tarteeb.Api/Startup.cs
+++ Tarteeb.Api/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.OpenApi.Models;
 using Tarteeb.Api.Brokers.DateTimes;
 using Tarteeb.Api.Brokers.Loggings;
 using Tarteeb.Api.Brokers.Storages;
+using Tarteeb.Api.Services.Foundations.Teams;
 using Tarteeb.Api.Services.Foundations.Tickets;
 
 namespace Tarteeb.Api
@@ -29,6 +30,7 @@ namespace Tarteeb.Api
             services.AddDbContext<StorageBroker>();
             RegisterBrokers(services);
             services.AddTransient<ITicketService, TicketService>();
+            services.AddTransient<ITeamService, TeamService>();
 
             services.AddSwaggerGen(config =>
             {

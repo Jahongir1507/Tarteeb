553b9464967c6f7726940a16f21541de7b201e3a
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-21 01:31:14 -0800

    ShouldThrowValidationExceptionOnModifyIfTeamIsInvalidAndLogItAsync -> FAIL

--- Tarteeb.Api.Tests.Unit/Services/Foundations/Teams/TeamServiceTests.Validations.Modify.cs
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Teams/TeamServiceTests.Validations.Modify.cs
@@ -46,5 +46,76 @@ namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Teams
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfTeamIsInvalidAndLogItAsync(string invalidString)
+        {
+            // given 
+            var invalidTeam = new Team
+            {
+                TeamName = invalidString
+            };
+
+            var invalidTeamException =
+                new InvalidTeamException();
+
+            invalidTeamException.AddData(
+                key: nameof(Team.Id),
+                values: "Id is required");
+
+            invalidTeamException.AddData(
+                key: nameof(Team.TeamName),
+                values: "Text is required");
+
+            invalidTeamException.AddData(
+                key: nameof(Team.CreatedDate),
+                values: "Date is required");
+
+            invalidTeamException.AddData(
+                key: nameof(Team.UpdatedDate),
+                    values: new[]
+                    {
+                        "Date is required",
+                        $"Date is the same as {nameof(Team.CreatedDate)}",
+                        "Date is not recent"
+                    }
+                );
+
+            var expectedTeamValidationException =
+                new TeamValidationException(invalidTeamException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTime())
+                    .Returns(GetRandomDateTime);
+
+            // when
+            ValueTask<Team> modifyTeamTask =
+                this.teamService.ModifyTeamAsync(invalidTeam);
+
+            TeamValidationException actualTeamValidationException =
+                await Assert.ThrowsAsync<TeamValidationException>(
+                    modifyTeamTask.AsTask);
+
+            //then
+            actualTeamValidationException.Should()
+                .BeEquivalentTo(expectedTeamValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedTeamValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateTeamAsync(It.IsAny<Team>()), Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }

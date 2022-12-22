05313445626baaf6a047c0a450e7b4cb21d0e1c0
iCloud <dadabaev027@gmail.com>
2022-12-22 16:49:36 +0500

    ShouldThrowValidationExceptionOnRemoveIfIdIsInvalidAndLogItAsync -> FAIL

---
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Tickets/TicketServiceTests.Validations.RemoveById.cs
@@ -0,0 +1,61 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarteeb.Api.Models.Tickets;
+using Tarteeb.Api.Models.Tickets.Exceptions;
+using Xunit;
+
+namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Tickets
+{
+    public partial class TicketServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRemoveIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid invalidTicketId = Guid.Empty;
+
+            var invalidTicketException =
+                new InvalidTicketException();
+
+            invalidTicketException.AddData(
+                key: nameof(Ticket.Id),
+                values: "Id is required");
+
+            var expectedTicketValidationException =
+                new TicketValidationException(invalidTicketException);
+
+            // when
+            ValueTask<Ticket> removeTicketByIdTask =
+                this.ticketService.RemoveTicketByIdAsync(invalidTicketId);
+
+            TicketValidationException actualTicketValidationException =
+                await Assert.ThrowsAsync<TicketValidationException>(
+                    removeTicketByIdTask.AsTask);
+
+            //then
+            actualTicketValidationException.Should().BeEquivalentTo(expectedTicketValidationException);
+
+            this.loggingBrokerMock.Verify(broker => 
+                broker.LogError(It.Is(SameExceptionAs(expectedTicketValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteTicketAsync(It.IsAny<Ticket>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

59fb40269b2b566039b3df7f963f1b83fe7c39af
Muhammad Muso <musogulomov123@gmail.com>
2022-12-22 17:05:22 +0500

    ShouldThrowNotFoundExceptionOnRemoveIfTicketIsNotFoundAndLogItAsync-> FAIL

--- Tarteeb.Api.Tests.Unit/Services/Foundations/Tickets/TicketServiceTests.Validations.RemoveById.cs
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Tickets/TicketServiceTests.Validations.RemoveById.cs
@@ -3,13 +3,10 @@
 // Free to use to bring order in your workplace
 //=================================
 
+using System;
+using System.Threading.Tasks;
 using FluentAssertions;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Tarteeb.Api.Models.Tickets;
 using Tarteeb.Api.Models.Tickets.Exceptions;
 using Xunit;
@@ -45,7 +42,7 @@ namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Tickets
             //then
             actualTicketValidationException.Should().BeEquivalentTo(expectedTicketValidationException);
 
-            this.loggingBrokerMock.Verify(broker => 
+            this.loggingBrokerMock.Verify(broker =>
                 broker.LogError(It.Is(SameExceptionAs(expectedTicketValidationException))),
                     Times.Once);
 
@@ -55,6 +52,51 @@ namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Tickets
 
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowNotFoundExceptionOnRemoveIfTicketIsNotFoundAndLogItAsync()
+        {
+            //given
+            Guid randomTicketId = Guid.NewGuid();
+            Guid inputTicketId = randomTicketId;
+            Ticket noTicket = null;
+
+            var notFoundTicketException =
+                new NotFoundTicketException(inputTicketId);
+
+            var expectedTicketValidationException =
+                new TicketValidationException(notFoundTicketException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectTicketByIdAsync(It.IsAny<Guid>()))
+                    .ReturnsAsync(noTicket);
+
+            //when
+            ValueTask<Ticket> removeTicketByIdTask =
+                this.ticketService.RemoveTicketByIdAsync(inputTicketId);
+
+            TicketValidationException actualTicketValidationException =
+                await Assert.ThrowsAsync<TicketValidationException>(
+                    removeTicketByIdTask.AsTask);
+
+            //then
+            actualTicketValidationException.Should()
+                .BeEquivalentTo(expectedTicketValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectTicketByIdAsync(It.IsAny<Guid>()), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedTicketValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+              broker.DeleteTicketAsync(It.IsAny<Ticket>()), Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
     }

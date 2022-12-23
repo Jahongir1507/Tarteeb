56f9c0e9c6be30660ffbfb5aff0a0e2b6a66ef05
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-23 12:04:58 -0800

    ShouldThrowDependencyValidationOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync -> PASS

---
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Teams/TeamServiceTests.Exception.RemoveById.cs
@@ -0,0 +1,65 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Tarteeb.Api.Models.Teams;
+using Tarteeb.Api.Models.Teams.Exceptions;
+using Xunit;
+
+namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Teams
+{
+    public partial class TeamServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyValidationOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someTeamId = Guid.NewGuid();
+
+            var databaseUpdateConcurrencyException =
+                new DbUpdateConcurrencyException();
+
+            var lockedTeamException =
+                new LockedTeamException(databaseUpdateConcurrencyException);
+
+            var expectedTeamDependencyValidationException =
+                new TeamDependencyValidationException(lockedTeamException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectTeamByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(databaseUpdateConcurrencyException);
+
+            // when
+            ValueTask<Team> removeTeamByIdTask =
+                this.teamService.RemoveTeamByIdAsync(someTeamId);
+
+            TeamDependencyValidationException actualTeamDependencyValidationException =
+                await Assert.ThrowsAsync<TeamDependencyValidationException>(
+                    removeTeamByIdTask.AsTask);
+
+            // then
+            actualTeamDependencyValidationException.Should().BeEquivalentTo(
+                expectedTeamDependencyValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectTeamByIdAsync(It.IsAny<Guid>()), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+            expectedTeamDependencyValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteTeamAsync(It.IsAny<Team>()), Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

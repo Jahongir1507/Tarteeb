1fe2a988131ea4ef201019ba16be28847ce7d1e1
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-29 03:29:11 -0800

    CODE RUB: Update Method

--- Tarteeb.Api.Tests.Unit/Services/Foundations/Teams/TeamServiceTests.Logic.Modify.cs
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Teams/TeamServiceTests.Logic.Modify.cs
@@ -18,37 +18,39 @@ namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Teams
         [Fact]
         public async Task ShouldModifyTeamAsync()
         {
-            // given
+            //given
             DateTimeOffset randomDate = GetRandomDateTime();
             Team randomTeam = CreateRandomModifyTeam(randomDate);
             Team inputTeam = randomTeam;
             Team storageTeam = inputTeam.DeepClone();
             storageTeam.UpdatedDate = randomTeam.CreatedDate;
             Team updatedTeam = inputTeam;
-            Team expectedTeam = updatedTeam;
-            Guid teamId = inputTeam.Id;
+            Team exceptedTeam = updatedTeam.DeepClone();
+            Guid TeamId = inputTeam.Id;
 
             this.dateTimeBrokerMock.Setup(broker =>
                 broker.GetCurrentDateTime()).Returns(randomDate);
 
             this.storageBrokerMock.Setup(broker =>
-                broker.SelectTeamByIdAsync(teamId)).ReturnsAsync(storageTeam);
+                broker.SelectTeamByIdAsync(TeamId))
+                    .ReturnsAsync(storageTeam);
 
             this.storageBrokerMock.Setup(broker =>
-                broker.UpdateTeamAsync(inputTeam)).ReturnsAsync(updatedTeam);
+                broker.UpdateTeamAsync(inputTeam))
+                    .ReturnsAsync(updatedTeam);
 
-            // when
+            //when
             Team actualTeam =
                 await this.teamService.ModifyTeamAsync(inputTeam);
 
-            // then
-            actualTeam.Should().BeEquivalentTo(expectedTeam);
+            //then
+            actualTeam.Should().BeEquivalentTo(exceptedTeam);
 
             this.dateTimeBrokerMock.Verify(broker =>
                 broker.GetCurrentDateTime(), Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
-                broker.SelectTeamByIdAsync(teamId), Times.Once);
+                broker.SelectTeamByIdAsync(TeamId), Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
                 broker.UpdateTeamAsync(inputTeam), Times.Once);

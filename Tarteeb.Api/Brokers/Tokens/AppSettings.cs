d103b7f9729649f8fd3a9bf2daea918961622abe
bekzod <jbekzod2004@gmail.com>
2023-01-21 16:55:38 +0500

    BROKERS: Changed AppSettingModel To JWTOptionsModel.

--- Tarteeb.Api/Brokers/Tokens/AppSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Tarteeb.Api.Brokers.Tokens
-{
-    public class AppSettings
-    {
-        public string SecretKey { get; set; }
-    }
-}
---
+++ Tarteeb.Api/Brokers/Tokens/JWTOptionsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Tarteeb.Api.Brokers.Tokens
+{
+    public class JWTOptionsModel
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public int ExpirationInMinutes { get; set; }
+        [NotMapped]
+        public string SecretKey { get => Environment.GetEnvironmentVariable("SECRET_KEY"); }
+    }
+}

f689a2b01e32063bfb8d75ddb0dde3e64b679d4e
bekzod <jbekzod2004@gmail.com>
2023-01-21 16:50:52 +0500

    CODE RUB: JWT options gives configuration. Set up other Claims Data.

--- Tarteeb.Api/Brokers/Tokens/TokenBroker.cs
+++ Tarteeb.Api/Brokers/Tokens/TokenBroker.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using Microsoft.Extensions.Configuration;
 using Tarteeb.Api.Models;
 using Tarteeb.Api.Services.Orchestrations.Model;
 
@@ -12,34 +13,35 @@ namespace Tarteeb.Api.Brokers.Tokens
 {
     public class TokenBroker
     {
-        private readonly IOptions<AppSettings> appSettings;
+        private readonly IConfiguration configuration;
 
-        public TokenBroker(IOptions<AppSettings> appSettings)=>
-            this.appSettings = appSettings;
+        public TokenBroker(IConfiguration configuration) =>
+            this.configuration = configuration;
 
         public string GenerateJWT(User user)
         {
-           var securityTokenHandler= new JwtSecurityTokenHandler();
+            JWTOptionsModel jwtOptionsModel = this.configuration.GetSection("JWTOptionsModel").Get<JWTOptionsModel>();
+            var securityTokenHandler = new JwtSecurityTokenHandler();
 
-           var claims =new List<Claim>
+            var claims = new List<Claim>
             {
-                new Claim(ClaimTypes.Email, user.Email),
-                new Claim(ClaimTypes.Hash,user.Password)
+                new Claim(ClaimTypes.Name, user.FirstName),
+                new Claim(ClaimTypes.Email, user.Email)
             };
 
-            var key = Encoding.ASCII.GetBytes(appSettings.Value.SecretKey);
+            var key = Encoding.ASCII.GetBytes(jwtOptionsModel.SecretKey);
 
             var tokenDescriptor = new SecurityTokenDescriptor()
             {
                 Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.UtcNow.AddDays(1),
+                Expires = DateTime.UtcNow.AddMinutes(jwtOptionsModel.ExpirationInMinutes),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
-                SecurityAlgorithms.HmacSha256Signature),
+                    SecurityAlgorithms.HmacSha256Signature),
+                Audience = jwtOptionsModel.Audience,
+                Issuer = jwtOptionsModel.Issuer
             };
-            var securityToken=securityTokenHandler.CreateToken(tokenDescriptor);
+            var securityToken = securityTokenHandler.CreateToken(tokenDescriptor);
             return securityTokenHandler.WriteToken(securityToken);
-
         }
     }
-
 }

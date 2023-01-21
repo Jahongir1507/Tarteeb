52408516e5de60f5adc9db09b738b73e5f8c95af
bekzod <jbekzod2004@gmail.com>
2023-01-21 16:46:48 +0500

    API: Added Processing and Orchestration services.

--- Tarteeb.Api/Startup.cs
+++ Tarteeb.Api/Startup.cs
@@ -3,6 +3,7 @@
 // Free to use to bring order in your workplace
 //=================================
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -12,9 +13,13 @@ using Microsoft.OpenApi.Models;
 using Tarteeb.Api.Brokers.DateTimes;
 using Tarteeb.Api.Brokers.Loggings;
 using Tarteeb.Api.Brokers.Storages;
+using Tarteeb.Api.Brokers.Tokens;
+using Tarteeb.Api.Services.Foundations;
 using Tarteeb.Api.Services.Foundations.Teams;
 using Tarteeb.Api.Services.Foundations.Tickets;
 using Tarteeb.Api.Services.Foundations.Users;
+using Tarteeb.Api.Services.Orchestrations;
+using Tarteeb.Api.Services.Processings;
 
 namespace Tarteeb.Api
 {
@@ -31,7 +36,9 @@ namespace Tarteeb.Api
             services.AddDbContext<StorageBroker>();
             RegisterBrokers(services);
             AddFoundationServices(services);
-
+            AddProcessingService(services);
+            AddOrchestrationService(services);
+            
             services.AddSwaggerGen(config =>
             {
                 config.SwaggerDoc(
@@ -39,7 +46,7 @@ namespace Tarteeb.Api
                     info: new OpenApiInfo { Title = "Tarteeb.Api", Version = "v1" });
             });
         }
-
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
         {
             if (environment.IsDevelopment())
@@ -53,9 +60,9 @@ namespace Tarteeb.Api
             }
 
             app.UseHttpsRedirection();
+            app.UseAuthentication();
             app.UseRouting();
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
                 endpoints.MapControllers());
         }
@@ -65,6 +72,8 @@ namespace Tarteeb.Api
             services.AddTransient<IStorageBroker, StorageBroker>();
             services.AddTransient<ILoggingBroker, LoggingBroker>();
             services.AddTransient<IDateTimeBroker, DateTimeBroker>();
+
+            services.AddScoped(typeof(TokenBroker));
         }
 
         private static void AddFoundationServices(IServiceCollection services)
@@ -72,6 +81,19 @@ namespace Tarteeb.Api
             services.AddTransient<ITicketService, TicketService>();
             services.AddTransient<IUserService, UserService>();
             services.AddTransient<ITeamService, TeamService>();
+
+            services.AddScoped(typeof(SecurityService));
         }
+        private void AddOrchestrationService(IServiceCollection services)
+        {
+            services.AddTransient<IUserOrchestrationService, UserOrchestrationService>();
+        }
+        
+        private void AddProcessingService(IServiceCollection services)
+        {
+            services.AddScoped(typeof(UserSecurityService));
+            services.AddScoped(typeof(UserProcessingService));
+        }
+
     }
 }

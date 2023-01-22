838507237c1a90ebbc2cbe6f36d51945cb046e51
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2023-01-22 09:53:28 +0300

    PROCESSINGS: Retrieve User By Credentials

---
+++ Tarteeb.Api/Services/Processings/Users/IUserProcessingService.cs
@@ -0,0 +1,14 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using Tarteeb.Api.Models;
+
+namespace Tarteeb.Api.Services.Processings.Users
+{
+    public interface IUserProcessingService
+    {
+        public User RetrieveUserByCredentails(string email,string password);
+    }
+}
---
+++ Tarteeb.Api/Services/Processings/Users/UserProcessingService.cs
@@ -0,0 +1,23 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using Tarteeb.Api.Models;
+using Tarteeb.Api.Services.Foundations.Users;
+
+namespace Tarteeb.Api.Services.Processings.Users
+{
+    public class UserProcessingService:IUserProcessingService
+    {
+        private readonly IUserService userService;
+
+        public UserProcessingService(IUserService userService) =>
+            this.userService = userService;
+
+        public User RetrieveUserByCredentails(string email, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

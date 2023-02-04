7bc888a8b9ae60e2b888b2183be7842d11230f88
shumtakadiyor@gmail.com <shumtakadiyor@gmail.com>
2023-02-04 09:57:09 +0500

    ShouldCreateToken->PASS

---
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/SecurityServiceTests.Logic.Create.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Moq;
+using Tarteeb.Api.Models;
+using Xunit;
+
+namespace Tarteeb.Api.Tests.Unit.Services.Foundations
+{
+    public partial class SecurityServiceTests
+    {
+        [Fact]
+        public void ShouldCreateToken()
+        {
+            //given
+            User randomUser = CreateRandomUser();
+            User inputUser = randomUser;
+            string randomString = "asadsdsfdfdfdfdfd";
+            string createdToken = randomString;
+            string expectedToken = createdToken;
+
+            this.tokenBrokerMock.Setup(broker =>
+                broker.GenerateJWT(inputUser)).Returns(expectedToken);
+
+            //when
+            string actualToken = securityService.CreateToken(inputUser);
+
+            //then
+            actualToken.Should().BeEquivalentTo(expectedToken);
+
+            this.tokenBrokerMock.Verify(broker =>
+                broker.GenerateJWT(inputUser), Times.Once);
+
+            this.tokenBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/SecurityServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using Moq;
+using System;
+using Tarteeb.Api.Brokers.Loggings;
+using Tarteeb.Api.Brokers.Tokens;
+using Tarteeb.Api.Models;
+using Tarteeb.Api.Services.Foundations;
+using Tynamix.ObjectFiller;
+
+namespace Tarteeb.Api.Tests.Unit.Services.Foundations
+{
+    public partial class SecurityServiceTests
+    {
+        private readonly Mock<ITokenBroker> tokenBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly ISecurityService securityService;
+
+
+        public SecurityServiceTests()
+        {
+            this.tokenBrokerMock = new Mock<ITokenBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.securityService = new SecurityService(
+                tokenBrokerMock.Object,
+                loggingBrokerMock.Object);
+        }
+        private static User CreateRandomUser() =>
+            CreateUserFiller(dates: GetRandomDateTimeOffset()).Create();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+          new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static Filler<User> CreateUserFiller(DateTimeOffset dates)
+        {
+            var filler = new Filler<User>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dates);
+
+            return filler;
+        }
+    }
+}

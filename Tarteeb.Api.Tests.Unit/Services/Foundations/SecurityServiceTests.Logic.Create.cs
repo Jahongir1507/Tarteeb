5e0be8c2e89d7270a262737e3ce2606f3d628c45
shumtakadiyor@gmail.com <shumtakadiyor@gmail.com>
2023-02-04 10:53:38 +0500

    ShouldCreateToken->PASS

---
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/SecurityServiceTests.Logic.Create.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarteeb.Api.Models;
+using Xunit;
+
+namespace Tarteeb.Api.Tests.Unit.Services.Foundations
+{
+    public partial class SecurityServiceTests
+    {
+        [Fact]
+        public void ShouldCreateToken()
+        {
+            //given
+            User randomUser = CreateRandomUser();
+            User inputUser = randomUser;
+            string randomString = CreateRandomString();
+            string createdToken = randomString;
+            string expectedToken = createdToken;
+
+            this.tokenBrokerMock.Setup(broker =>
+                broker.GenerateJWT(inputUser)).Returns(expectedToken);
+
+            //when
+            string actualToken = securityService.CreateToken(inputUser);
+
+            //then
+            actualToken.Should().BeEquivalentTo(expectedToken);
+
+            this.tokenBrokerMock.Verify(broker =>
+                broker.GenerateJWT(inputUser), Times.Once);
+
+            this.tokenBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+    }
+}
---
+++ Tarteeb.Api/Services/Foundations/SecurityService.cs
@@ -0,0 +1,25 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using Tarteeb.Api.Brokers.Loggings;
+using Tarteeb.Api.Brokers.Tokens;
+using Tarteeb.Api.Models;
+
+namespace Tarteeb.Api.Services.Foundations;
+
+public class SecurityService : ISecurityService
+{
+    private readonly ITokenBroker tokenBroker;
+    private readonly ILoggingBroker loggingBroker;
+
+    public SecurityService(ITokenBroker tokenBroker, ILoggingBroker loggingBroker)
+    {
+        this.tokenBroker = tokenBroker;
+        this.loggingBroker = loggingBroker;
+    }
+
+    public string CreateToken(User user) =>
+        tokenBroker.GenerateJWT(user);
+}

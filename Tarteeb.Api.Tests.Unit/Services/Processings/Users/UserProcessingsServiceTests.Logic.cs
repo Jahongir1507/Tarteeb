047f31f4c8bac33bd603b130dd8810cec7de634c
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2023-02-22 08:22:40 +0300

    ShoulRetrieveUserByCredentails -> FAIL

---
+++ Tarteeb.Api.Tests.Unit/Services/Processings/Users/UserProcessingsServiceTests.Logic.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tarteeb.Api.Models;
+using Xunit;
+
+namespace Tarteeb.Api.Tests.Unit.Services.Processings.Users
+{
+    public partial class UserProcessingsServiceTests
+    {
+        [Fact]
+        public async Task ShoulRetrieveUserByCredentails()
+        {
+            // given
+            string inputEmail = GetrandomString();
+            string inputPassword = GetrandomString();
+            var expectedUser = new User { Email = inputEmail, Password = inputPassword };
+            var users = new List<User> { expectedUser };
+
+            this.userServiceMock.Setup(service =>
+                service.RetrieveAllUsers())
+                    .Returns(users.AsQueryable());
+
+            // when
+            var actualUser = await userProcessingsService.RetrieveUserByCredentails(inputEmail, inputPassword);
+
+            // then
+            actualUser.Should().BeEquivalentTo(expectedUser);
+
+            this.userServiceMock.Verify(service =>
+                service.RetrieveAllUsers(), Times.Once);
+
+            this.userServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ Tarteeb.Api.Tests.Unit/Services/Processings/Users/UserProcessingsServiceTests.cs
@@ -0,0 +1,36 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using Moq;
+using Tarteeb.Api.Brokers.Loggings;
+using Tarteeb.Api.Services.Foundations.Users;
+using Tarteeb.Api.Services.Processings.Users;
+using Tynamix.ObjectFiller;
+
+namespace Tarteeb.Api.Tests.Unit.Services.Processings.Users
+{
+    public partial class UserProcessingsServiceTests
+    {
+        private readonly Mock<IUserService> userServiceMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IUserProcessingService userProcessingsService;
+
+        public UserProcessingsServiceTests()
+        {
+            this.userServiceMock = new Mock<IUserService>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.userProcessingsService = new UserProcessingService(
+                userService: this.userServiceMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static string GetrandomString() =>
+            new MnemonicString(wordCount: GetRandomNumber()).GetValue();
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+    }
+}

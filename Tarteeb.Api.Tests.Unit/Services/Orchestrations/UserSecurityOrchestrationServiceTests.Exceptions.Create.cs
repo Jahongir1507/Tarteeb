ec4ff0c1e25d21b181a86eb814056b4b622424d9
Elbek <e.normurodov@mail.ru>
2023-03-17 09:12:35 -0700

    ShoudThrowServiceExceptionOnCreateIfServiceErrorOccursAndLogItAsync -> FAIL

--- Tarteeb.Api.Tests.Unit/Services/Orchestrations/UserSecurityOrchestrationServiceTests.Exceptions.Create.cs
+++ Tarteeb.Api.Tests.Unit/Services/Orchestrations/UserSecurityOrchestrationServiceTests.Exceptions.Create.cs
@@ -50,5 +50,44 @@ namespace Tarteeb.Api.Tests.Unit.Services.Orchestrations
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.securityServiceMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public void ShoudThrowServiceExceptionOnCreateIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            string someString = GetRandomString();
+            var serviceException = new Exception();
+
+            var failedUserTokenOrchestrationException = new 
+                FailedUserTokenOrchestrationException(serviceException);
+
+            var expectedUserTokenOrchestrationServiceException =
+                new UserTokenOrchestrationServiceException(failedUserTokenOrchestrationException);
+
+            this.userServiceMock.Setup(service => service.RetrieveAllUsers())
+                .Throws(serviceException);
+
+            // when
+            Action createTokenAction = () =>
+                this.userSecurityOrchestrationService.CreateUserToken(email: someString, password: someString);
+
+            UserTokenOrchestrationServiceException actualUserTokenOrchestrationServiceException =
+                Assert.Throws<UserTokenOrchestrationServiceException>(createTokenAction);
+
+            // then
+            actualUserTokenOrchestrationServiceException.Should().BeEquivalentTo(
+                expectedUserTokenOrchestrationServiceException);
+
+            this.userServiceMock.Verify(service => service.RetrieveAllUsers(), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedUserTokenOrchestrationServiceException))),
+                        Times.Once);
+
+            this.userServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.securityServiceMock.VerifyNoOtherCalls();
+        }
     }
 }
---
+++ Tarteeb.Api/Models/Orchestrations/UserTokens/Exceptions/FailedUserTokenOrchestrationException.cs
@@ -0,0 +1,17 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System;
+using Xeptions;
+
+namespace Tarteeb.Api.Models.Orchestrations.UserTokens.Exceptions
+{
+    public class FailedUserTokenOrchestrationException: Xeption
+    {
+        public FailedUserTokenOrchestrationException(Exception innerException)
+            : base(message: "Failed user token service error occurred, contact support.", innerException)
+        { }
+    }
+}

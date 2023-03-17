35306d86e4df9e2738ebf6552a6d151a684b937a
ElbekDeveloper <e.normurodov@mail.ru>
2023-03-17 20:33:48 +0500

    ShouldThrowValidationExceptionOnCreateIfEmailOrPasswordAreInvalidAndLogItAsync -> WIP

---
+++ Tarteeb.Api/Services/Orchestrations/UserSecurityOrchestrationService.Exceptions.cs
@@ -0,0 +1,37 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System.Security.Authentication;
+using Tarteeb.Api.Models.Orchestrations.UserTokens;
+using Tarteeb.Api.Models.Orchestrations.UserTokens.Exceptions;
+using Xeptions;
+
+namespace Tarteeb.Api.Services.Orchestrations
+{
+    public partial class UserSecurityOrchestrationService
+    {
+        private delegate UserToken ReturningUserTokenFunction();
+
+        private UserToken TryCatch(ReturningUserTokenFunction returningUserTokenFunction)
+        {
+            try
+            {
+                return returningUserTokenFunction();
+            }
+            catch (InvalidUserCreadentialOrchestrationException invalidUserCreadentialOrchestrationException)
+            {
+                throw CreateAndLogValidationException(invalidUserCreadentialOrchestrationException);
+            }
+        }
+
+        private UserTokenOrchestrationValidationException CreateAndLogValidationException(Xeption exception)
+        {
+            var userTokenOrchestrationValidationException = new UserTokenOrchestrationValidationException(exception);
+            this.loggingBroker.LogError(userTokenOrchestrationValidationException);
+
+            return userTokenOrchestrationValidationException;
+        }
+    }
+}

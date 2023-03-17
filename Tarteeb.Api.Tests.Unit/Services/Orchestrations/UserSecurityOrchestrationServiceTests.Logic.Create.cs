a8b607dd8a019368bd72b1ddcac6efa1caa34bb4
ElbekDeveloper <e.normurodov@mail.ru>
2023-03-17 18:36:56 +0500

    ShoudCreateUserToken -> WIP

---
+++ Tarteeb.Api.Tests.Unit/Services/Orchestrations/UserSecurityOrchestrationServiceTests.Logic.Create.cs
@@ -0,0 +1,22 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using Xunit;
+
+namespace Tarteeb.Api.Tests.Unit.Services.Orchestrations
+{
+    public partial class UserSecurityOrchestrationServiceTests
+    {
+        [Fact]
+        public void ShoudCreateUserToken()
+        {
+            // given
+
+            // when
+
+            // then
+        }
+    }
+}
---
+++ Tarteeb.Api.Tests.Unit/Services/Orchestrations/UserSecurityOrchestrationServiceTests.cs
@@ -0,0 +1,33 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using Moq;
+using Tarteeb.Api.Brokers.Loggings;
+using Tarteeb.Api.Services.Foundations.Securities;
+using Tarteeb.Api.Services.Foundations.Users;
+using Tarteeb.Api.Services.Orchestrations;
+
+namespace Tarteeb.Api.Tests.Unit.Services.Orchestrations
+{
+    public class UserSecurityOrchestrationServiceTests
+    {
+        private readonly Mock<IUserService> userServiceMock;
+        private readonly Mock<ISecurityService> securityServiceMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IUserSecurityOrchestrationService userSecurityOrchestrationService;
+
+        public UserSecurityOrchestrationServiceTests()
+        {
+            userServiceMock = new Mock<IUserService>();
+            securityServiceMock = new Mock<ISecurityService>();
+            loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.userSecurityOrchestrationService = new UserSecurityOrchestrationService(
+                userService: userServiceMock.Object,
+                securityService: securityServiceMock.Object,
+                loggingBroker: loggingBrokerMock.Object);
+        }
+    }
+}

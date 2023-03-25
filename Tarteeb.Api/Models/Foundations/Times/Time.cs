9a0bed268aa163b0e38f98a87445740c537cbaa3
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-25 14:55:32 +0500

    DATA : Model & Migrate Time

---
+++ Tarteeb.Api/Models/Foundations/Times/Time.cs
@@ -0,0 +1,24 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System;
+using Tarteeb.Api.Models.Foundations.Tickets;
+using Tarteeb.Api.Models.Foundations.Users;
+
+namespace Tarteeb.Api.Models.Foundations.Times
+{
+    public class Time
+    {
+        public Guid Id { get; set; }
+        public decimal HoursWorked { get; set; }
+        public string Comment { get; set; }
+        public Guid UserId { get; set; }
+        public Guid TicketId { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+        public Ticket Ticket { get; set; }
+        public User User { get; set; }
+    }
+}

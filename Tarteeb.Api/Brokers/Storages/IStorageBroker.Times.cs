e7b467234d063e834f7173a27967b976f1f590f1
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-03-25 20:20:35 +0500

    BROKERS: Select Time By Id

--- Tarteeb.Api/Brokers/Storages/IStorageBroker.Times.cs
+++ Tarteeb.Api/Brokers/Storages/IStorageBroker.Times.cs
@@ -3,8 +3,15 @@
 // Free to use to bring order in your workplace
 //=================================
 
+using System.Threading.Tasks;
+using System;
+using Tarteeb.Api.Models.Foundations.Tickets;
+using Tarteeb.Api.Models.Foundations.Times;
+
 namespace Tarteeb.Api.Brokers.Storages
 {
     public partial interface IStorageBroker
-    {}
+    {
+        ValueTask<Time> SelectTimeByIdAsync(Guid id);
+    }
 }
--- Tarteeb.Api/Brokers/Storages/StorageBroker.Times.cs
+++ Tarteeb.Api/Brokers/Storages/StorageBroker.Times.cs
@@ -3,7 +3,10 @@
 // Free to use to bring order in your workplace
 //=================================
 
+using System.Threading.Tasks;
+using System;
 using Microsoft.EntityFrameworkCore;
+using Tarteeb.Api.Models.Foundations.Tickets;
 using Tarteeb.Api.Models.Foundations.Times;
 
 namespace Tarteeb.Api.Brokers.Storages
@@ -11,5 +14,8 @@ namespace Tarteeb.Api.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<Time> Times { get; set; }
+
+        public async ValueTask<Time> SelectTimeByIdAsync(Guid id) =>
+          await SelectAsync<Time>(id);
     }
 }

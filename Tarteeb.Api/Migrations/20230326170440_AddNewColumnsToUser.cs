ffc3c78046dfab26d32020b189e6a510b1e268c1
jahongir1507 <ishankulovjahongir1507@gmail.com>
2023-03-26 22:08:53 +0500

    MIGRATE: Migrated New Columns

---
+++ Tarteeb.Api/Migrations/20230326170440_AddNewColumnsToUser.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tarteeb.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewColumnsToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GitHubUsername",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "IsActive",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "IsVerififed",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TelegramUsername",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GitHubUsername",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsVerififed",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TelegramUsername",
+                table: "Users");
+        }
+    }
+}

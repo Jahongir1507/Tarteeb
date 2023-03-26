3d59c1a75b2086e5dc81e2a859da3d0222f3b95d
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-03-26 22:28:54 +0500

    CODE RUB: Add Header Files

--- Tarteeb.Api/Brokers/Storages/IStorageBroker.Scores.cs
+++ Tarteeb.Api/Brokers/Storages/IStorageBroker.Scores.cs
@@ -1,4 +1,9 @@
-﻿namespace Tarteeb.Api.Brokers.Storages
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+namespace Tarteeb.Api.Brokers.Storages
 {
     public partial interface IStorageBroker
     {}

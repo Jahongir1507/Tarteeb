f680ee3970ea1d2813af2e875241e373fa87fa98
KholmatovKhabibulla <85988278+khabibulladev@users.noreply.github.com>
2023-03-27 12:40:07 +0500

    BROKERS: Delete Score

--- Tarteeb.Api/Brokers/Storages/IStorageBroker.Scores.cs
+++ Tarteeb.Api/Brokers/Storages/IStorageBroker.Scores.cs
@@ -3,8 +3,13 @@
 // Free to use to bring order in your workplace
 //=================================
 
+using System.Threading.Tasks;
+using Tarteeb.Api.Models.Foundations.Scores;
+
 namespace Tarteeb.Api.Brokers.Storages
 {
     public partial interface IStorageBroker
-    {}
+    {
+        ValueTask<Score> DeleteScoreAsync(Score score);
+    }
 }

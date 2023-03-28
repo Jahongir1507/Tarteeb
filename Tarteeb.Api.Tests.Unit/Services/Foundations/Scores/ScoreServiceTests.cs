e6f22074a63f0d4b15abc7f24c0f62268d32d431
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-03-28 15:18:24 +0500

    ShouldAddScoreAsync -> FAIL

---
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Scores/ScoreServiceTests.cs
@@ -0,0 +1,42 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System;
+using Moq;
+using Tarteeb.Api.Brokers.Storages;
+using Tarteeb.Api.Models.Foundations.Scores;
+using Tarteeb.Api.Services.Foundations.Scores;
+using Tynamix.ObjectFiller;
+
+namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Scores
+{
+    public partial class ScoreServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly IScoreService scoreService;
+        public ScoreServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.scoreService = new ScoreService(
+                storageBroker: storageBrokerMock.Object);
+        }
+
+        private DateTimeOffset GetRandomDateTime() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private Score CreateRandomScore() =>
+            CreateScoreFiller(GetRandomDateTime()).Create();
+
+        private Filler<Score> CreateScoreFiller(DateTimeOffset dates)
+        {
+            var filler = new Filler<Score>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dates);
+            
+            return filler;
+        }
+    }
+}
--- Tarteeb.Api/Services/Foundations/Scores/ScoreService.cs
+++ Tarteeb.Api/Services/Foundations/Scores/ScoreService.cs
@@ -5,15 +5,19 @@
 
 using System;
 using System.Threading.Tasks;
+using Tarteeb.Api.Brokers.Storages;
 using Tarteeb.Api.Models.Foundations.Scores;
 
 namespace Tarteeb.Api.Services.Foundations.Scores
 {
     public class ScoreService : IScoreService
     {
-        public ValueTask<Score> RetrieveScoreByIdAsync(Guid id)
+        private readonly IStorageBroker storageBroker;
+        public ScoreService(IStorageBroker storageBroker)
         {
-            throw new NotImplementedException();
+            this.storageBroker = storageBroker;
         }
+        public ValueTask<Score> RetrieveScoreByIdAsync(Guid id) =>
+            throw new NotImplementedException();
     }
 }

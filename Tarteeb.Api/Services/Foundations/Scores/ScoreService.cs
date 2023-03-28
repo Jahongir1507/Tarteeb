906367ab07eaeff8cef037371d5b6fa5cff51bac
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-03-28 13:29:56 +0500

    CODFIG: Implement in Startup.cs Foundations ScoreService

---
+++ Tarteeb.Api/Services/Foundations/Scores/ScoreService.cs
@@ -0,0 +1,19 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System;
+using System.Threading.Tasks;
+using Tarteeb.Api.Models.Foundations.Scores;
+
+namespace Tarteeb.Api.Services.Foundations.Scores
+{
+    public class ScoreService : IScoreService
+    {
+        public ValueTask<Score> RetrieveScoreByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

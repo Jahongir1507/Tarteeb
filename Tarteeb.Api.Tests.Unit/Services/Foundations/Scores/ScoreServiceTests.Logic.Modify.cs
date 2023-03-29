d0d8a80fc9251e869e5a0e391fc1368a854ad5c4
Xurshid-A-J <abdxurshid02@gmail.com>
2023-03-29 21:24:36 +0500

    ShouldModifyScoreAsync -> WIP

---
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Scores/ScoreServiceTests.Logic.Modify.cs
@@ -0,0 +1,28 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System;
+using System.Threading.Tasks;
+using Tynamix.ObjectFiller;
+using Xunit;
+
+namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Scores
+{
+    public partial class ScoreServiceTests
+    {
+        [Fact]
+        public async Task ShouldModifyScoreAsync()
+        {
+            // given
+
+
+            // when
+
+
+            // then 
+        }
+    }
+}
+
---
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Scores/ScoreServiceTests.cs
@@ -0,0 +1,35 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System;
+using Moq;
+using Tarteeb.Api.Brokers.DateTimes;
+using Tarteeb.Api.Brokers.Loggings;
+using Tarteeb.Api.Brokers.Storages;
+using Tarteeb.Api.Services.Foundations.Scores;
+
+namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Scores
+{
+    public class ScoreServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerBrokerMock;
+        private readonly IScoreService scoreService;
+
+        public ScoreServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.loggingBrokerBrokerMock = new Mock<ILoggingBroker>();
+
+            this.scoreService = new ScoreService(
+                this.storageBrokerMock.Object,
+                this.dateTimeBrokerMock.Object,
+                this.loggingBrokerBrokerMock.Object);
+        }
+    }
+}
+

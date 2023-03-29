fa3ec5f96cca642ad6a9fa0a65ff9b12d549ed56
Xurshid-A-J <abdxurshid02@gmail.com>
2023-03-29 23:37:54 +0500

    ShouldModifyScoreAsync -> FAIL

--- Tarteeb.Api.Tests.Unit/Services/Foundations/Scores/ScoreServiceTests.Logic.Modify.cs
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Scores/ScoreServiceTests.Logic.Modify.cs
@@ -5,6 +5,10 @@
 
 using System;
 using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Tarteeb.Api.Models.Foundations.Scores;
 using Tynamix.ObjectFiller;
 using Xunit;
 
@@ -16,12 +20,44 @@ namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Scores
         public async Task ShouldModifyScoreAsync()
         {
             // given
+            DateTimeOffset randomDate = GetRandomDateTimeOffset();
+            Score randomScore = CreateRandomScore(randomDate);
+            Score inputScore = randomScore;
+            inputScore.UpdatedDate = randomDate.AddMinutes(1);
+            Score storageScore = inputScore;
+            Score updatedScore = inputScore;
+            Score expectedScore = updatedScore.DeepClone();
+            Guid inputScoreId = inputScore.Id;
 
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTime()).Returns(randomDate);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectScoreByIdAsync(inputScoreId))
+                    .ReturnsAsync(storageScore);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.UpdateScoreAsync(inputScore))
+                    .ReturnsAsync(updatedScore);
 
             // when
+            Score actualScoreTask =
+                 await this.scoreService.ModifyScoreAsync(inputScore);
 
+            // then
+            actualScoreTask.Should().BeEquivalentTo(expectedScore);
 
-            // then 
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectScoreByIdAsync(inputScoreId), Times.Once);
+
+            this.storageBrokerMock.Verify(broker => broker.UpdateScoreAsync(inputScore), Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
         }
     }
 }
--- Tarteeb.Api.Tests.Unit/Services/Foundations/Scores/ScoreServiceTests.cs
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Scores/ScoreServiceTests.cs
@@ -8,27 +8,45 @@ using Moq;
 using Tarteeb.Api.Brokers.DateTimes;
 using Tarteeb.Api.Brokers.Loggings;
 using Tarteeb.Api.Brokers.Storages;
+using Tarteeb.Api.Models.Foundations.Scores;
 using Tarteeb.Api.Services.Foundations.Scores;
+using Tynamix.ObjectFiller;
 
 namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Scores
 {
-    public class ScoreServiceTests
+    public partial class ScoreServiceTests
     {
         private readonly Mock<IStorageBroker> storageBrokerMock;
         private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
-        private readonly Mock<ILoggingBroker> loggingBrokerBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
         private readonly IScoreService scoreService;
 
         public ScoreServiceTests()
         {
             this.storageBrokerMock = new Mock<IStorageBroker>();
             this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
-            this.loggingBrokerBrokerMock = new Mock<ILoggingBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
 
             this.scoreService = new ScoreService(
                 this.storageBrokerMock.Object,
                 this.dateTimeBrokerMock.Object,
-                this.loggingBrokerBrokerMock.Object);
+                this.loggingBrokerMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static Score CreateRandomScore(DateTimeOffset dateTimeOffset) =>
+            CreateScoreFiller(GetRandomDateTimeOffset()).Create();
+
+        private static Filler<Score> CreateScoreFiller(DateTimeOffset date)
+        {
+            var filler = new Filler<Score>();
+
+            filler.Setup().
+                OnType<DateTimeOffset>().Use(date);
+
+            return filler;
         }
     }
 }

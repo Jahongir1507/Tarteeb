967597558e645e2fd3885f8c161e3ca98ff5d306
khatanboyevelbek <programmerelbek@gmail.com>
2023-03-29 22:09:06 +0500

    BROKERS: Send Email Confirmation

---
+++ Tarteeb.Api/Brokers/Emails/EmailConfirmationBroker.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using PostmarkDotNet;
+using PostmarkDotNet.Model;
+
+namespace Tarteeb.Api.Brokers.Emails
+{
+    public class EmailConfirmationBroker : IEmailConfirmationBroker
+    {
+        private readonly IConfiguration Configuration;
+
+        public EmailConfirmationBroker(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public async Task<PostmarkResponse> SendingConfirmationMessage(
+            string userEmail, string messageHtmlBody)
+        {
+            string apiKey = 
+                this.Configuration.GetSection("Postmark").GetValue<string>("ApiKey");
+
+            string senderSignature = 
+                this.Configuration.GetSection("Postmark").GetValue<string>("SenderSignature");
+
+            var client = new PostmarkClient(apiKey);
+
+            var message = new PostmarkMessage()
+            {
+                To = userEmail,
+                From = senderSignature,
+                TrackOpens = true,
+                Subject = "Email Confirmation",
+                HtmlBody = messageHtmlBody
+            };
+
+            var result = await client.SendMessageAsync(message);
+            return result;
+        }
+    }
+}
---
+++ Tarteeb.Api/Brokers/Emails/IEmailConfirmationBroker.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using PostmarkDotNet;
+
+namespace Tarteeb.Api.Brokers.Emails
+{
+    public interface IEmailConfirmationBroker
+    {
+        Task<PostmarkResponse> SendingConfirmationMessage(
+            string userEmail, string messageHtmlBody);
+    }
+}

eaa844a6c1067cc75db0b3fc93642199ec760f17
Xurshid-A-J <abdxurshid02@gmail.com>
2023-04-01 18:29:16 +0500

    ShouldThrowValidationExceptionOnModifyIfScoreIsInvalidAndLogItAsync -> FAIL

--- Tarteeb.Api.Tests.Unit/Services/Foundations/Scores/ScoreServiceTests.Velidations.Modify.cs
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Scores/ScoreServiceTests.Velidations.Modify.cs
@@ -8,6 +8,7 @@ using Tarteeb.Api.Models.Foundations.Scores.Exceptions;
 using Tarteeb.Api.Models.Foundations.Scores;
 using Xunit;
 using FluentAssertions;
+using Tarteeb.Api.Models.Foundations.Tickets;
 
 namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Scores
 {
@@ -40,6 +41,86 @@ namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Scores
 
             this.storageBrokerMock.Verify(broker =>
                 broker.UpdateScoreAsync(It.IsAny<Score>()), Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfScoreIsInvalidAndLogItAsync(
+            string invalidString)
+        {
+            // given 
+            var invalidScore = new Score()
+            {
+                EffortLink = invalidString
+            };
+
+            var invalidScoreException = new InvalidScoreException();
+
+            invalidScoreException.AddData(
+                key: nameof(Score.Id),
+                values: "Id is required");
+
+            invalidScoreException.AddData(
+               key: nameof(Score.Grade),
+               values: "Grade is required");
+
+            invalidScoreException.AddData(
+               key: nameof(Score.Weight),
+               values: "Weight is required");
+
+            invalidScoreException.AddData(
+               key: nameof(Score.EffortLink),
+               values: "Text is required");
+
+            invalidScoreException.AddData(
+               key: nameof(Score.TicketId),
+               values: "Id is required");
+
+            invalidScoreException.AddData(
+               key: nameof(Score.UserId),
+               values: "Id is required");
+
+            invalidScoreException.AddData(
+               key: nameof(Score.CreatedDate),
+               values: "Date is required");
+
+            invalidScoreException.AddData(
+                key: nameof(Ticket.UpdatedDate),
+                 "Date is required",
+                "Date is not recent",
+                $"Date is the same as {nameof(Score.CreatedDate)}");
+
+            var expectedScoreValidationException =
+                new ScoreValidationException(invalidScoreException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTime()).Returns(GetRandomDateTime);
+
+            // when
+            ValueTask<Score> modifyScoreTask =
+                this.scoreService.ModifyScoreAsync(invalidScore);
+
+            ScoreValidationException actualScoreValidationException =
+                await Assert.ThrowsAsync<ScoreValidationException>(modifyScoreTask.AsTask);
+
+            // then
+            actualScoreValidationException.Should()
+                .BeEquivalentTo(expectedScoreValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedScoreValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                 broker.UpdateScoreAsync(It.IsAny<Score>()), Times.Never);
 
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
--- Tarteeb.Api/Services/Foundations/Scores/ScoreService.Validations.cs
+++ Tarteeb.Api/Services/Foundations/Scores/ScoreService.Validations.cs
@@ -6,15 +6,55 @@
 using System;
 using Tarteeb.Api.Models.Foundations.Scores;
 using Tarteeb.Api.Models.Foundations.Scores.Exceptions;
+using Tarteeb.Api.Models.Foundations.Times;
 
 namespace Tarteeb.Api.Services.Foundations.Scores
 {
     public partial class ScoreService
     {
+        private void ValidateTimeOnModify(Score score)
+        {
+            ValidateScoreNotNull(score);
+            Validate(
+                (Rule: IsInvalid(score.Id), nameof(Score.Id)),
+                (Rule: IsInvalid(score.Grade), nameof(Score.Grade)),
+                (Rule: IsInvalid(score.Weight), nameof(Score.Weight)),
+                (Rule: IsInvalid(score.EffortLink), nameof(Score.EffortLink)),
+                (Rule: IsInvalid(score.TicketId), nameof(Score.TicketId)),
+                (Rule: IsInvalid(score.UserId), nameof(Score.UserId)),
+                (Rule: IsInvalid(score.CreatedDate), nameof(Score.CreatedDate)),
+                (Rule: IsInvalid(score.UpdatedDate), nameof(Score.UpdatedDate)),
+                (Rule: IsInvalid(score.UpdatedDate), nameof(Score.UpdatedDate)));
+        }
+
         private static dynamic IsInvalid(Guid id) => new
         {
             Condition = id == default,
             Message = "Id is required"
+        };
+
+        private static dynamic IsInvalid(int grade) => new
+        {
+            Condition = grade == default,
+            Message = "Grade is required"
+        };
+
+        private static dynamic IsInvalid(float weight) => new
+        {
+            Condition = weight == default,
+            Message = "Weight is required"
+        };
+
+        private static dynamic IsInvalid(string effortLink) => new
+        {
+            Condition = String.IsNullOrWhiteSpace(effortLink),
+            Message = "Text is required"
+        };
+
+        private static dynamic IsInvalid(DateTimeOffset dates) => new
+        {
+            Condition = dates == default,
+            Message = "Date is required"
         };
 
         private static void ValidateStorageScoreExist(Score maybeScore, Guid scoreId)

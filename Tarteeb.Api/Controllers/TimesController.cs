8e5f50b1cd45eca699aca5be1d0887097160bea3
ElbekDeveloper <e.normurodov@mail.ru>
2023-04-01 15:25:37 +0500

    CONTROLLERS: Get All Time

---
+++ Tarteeb.Api/Controllers/TimesController.cs
@@ -0,0 +1,47 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using RESTFulSense.Controllers;
+using Tarteeb.Api.Models.Foundations.Tickets.Exceptions;
+using Tarteeb.Api.Models.Foundations.Tickets;
+using Tarteeb.Api.Services.Foundations.Times;
+using Tarteeb.Api.Models.Foundations.Times.Exceptions;
+using Tarteeb.Api.Models.Foundations.Times;
+
+namespace Tarteeb.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TimesController : RESTFulController
+    {
+        private readonly ITimeService timeService;
+
+        public TimesController(ITimeService timeService) =>
+            this.timeService = timeService;
+
+        [HttpGet]
+        [EnableQuery]
+        public ActionResult<IQueryable<Time>> GetAllTimes()
+        {
+            try
+            {
+                IQueryable<Time> allTimes = this.timeService.RetrieveAllTimes();
+
+                return Ok(allTimes);
+            }
+            catch (TimeDependencyException timeDependencyException)
+            {
+                return InternalServerError(timeDependencyException.InnerException);
+            }
+            catch (TimeServiceException timeServiceException)
+            {
+                return InternalServerError(timeServiceException.InnerException);
+            }
+        }
+    }
+}

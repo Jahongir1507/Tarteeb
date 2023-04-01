c6d2a6bde2d124d4eb6a82fd04e76a68642c3bcb
mohinur18 <muhammadjonovamohinur36@gmail.com>
2023-04-01 17:25:32 +0500

    ShouldThrowValidationExceptionOnModifyIfScoreIsNullAndLogItAsync->FAIL

---
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Scores/ScoreServiceTests.Velidations.Modify.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Tarteeb.Api.Models.Foundations.Scores.Exceptions;
+using Tarteeb.Api.Models.Foundations.Scores;
+using Xunit;
+using FluentAssertions;
+
+namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Scores
+{
+    public partial class ScoreServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfScoreIsNullAndLogItAsync()
+        {
+            // given
+            Score nullScore = null;
+            var nullScoreException = new NullScoreException();
+
+            var expectedScoreValidationException =
+                new ScoreValidationException(nullScoreException);
+
+            // when
+            ValueTask<Score> modifyScoreTask =
+                this.scoreService.ModifyScoreAsync(nullScore);
+
+            ScoreValidationException actualScoreValidationException =
+                await Assert.ThrowsAsync<ScoreValidationException>(modifyScoreTask.AsTask);
+
+            // then
+            actualScoreValidationException.Should()
+                .BeEquivalentTo(expectedScoreValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedScoreValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateScoreAsync(It.IsAny<Score>()), Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

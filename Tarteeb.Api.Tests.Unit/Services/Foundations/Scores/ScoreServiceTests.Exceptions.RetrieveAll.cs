83881825e6eef737b0aa69c91600046b5e98b724
mohinur18 <muhammadjonovamohinur36@gmail.com>
2023-04-02 00:59:57 +0500

    ShouldThrowServiceExceptionOnRetrieveAllWhenAllServiceErrorOccursAndLogIt -> FAIL

--- Tarteeb.Api.Tests.Unit/Services/Foundations/Scores/ScoreServiceTests.Exceptions.RetrieveAll.cs
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Scores/ScoreServiceTests.Exceptions.RetrieveAll.cs
@@ -46,5 +46,43 @@ namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Scores
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public void ShouldThrowServiceExceptionOnRetrieveAllWhenAllServiceErrorOccursAndLogIt()
+        {
+            // given
+            string exceptionMessage = GetRandomString();
+            var serviceException = new Exception(exceptionMessage);
+
+            var failedScoreServiceException =
+                new FailedScoreServiceException(serviceException);
+
+            var expectedScoreServiceException =
+                new ScoreServiceException(failedScoreServiceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllScores()).Throws(serviceException);
+
+            // when
+            Action retrieveAllScoreAction = () =>
+                this.scoreService.RetrieveAllScores();
+
+            ScoreServiceException actualScoreServiceException =
+                Assert.Throws<ScoreServiceException>(retrieveAllScoreAction);
+
+            // then
+            actualScoreServiceException.Should().BeEquivalentTo(expectedScoreServiceException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllScores(), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedScoreServiceException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }

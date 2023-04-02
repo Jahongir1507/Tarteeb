0fb01a5a29aa07885bb9943e736d68e415b0317e
Nodirkhan Abdumurotov <nodirxonabdumurotov@mail.ru>
2023-04-02 15:24:28 +0500

    ShouldThrowValidationExceptionOnAddIfCreatedDateIsNotSameAsUpdatedDateAndLogItAsync -> PASS

--- Tarteeb.Api/Services/Foundations/Scores/ScoreService.Validations.cs
+++ Tarteeb.Api/Services/Foundations/Scores/ScoreService.Validations.cs
@@ -19,7 +19,14 @@ namespace Tarteeb.Api.Services.Foundations.Scores
                 (Rule: IsInvalid(score.Id), Parameter: nameof(Score.Id)),
                 (Rule: IsInvalid(score.EffortLink), Parameter: nameof(Score.EffortLink)),
                 (Rule: IsInvalid(score.CreatedDate), Parameter: nameof(Score.CreatedDate)),
-                (Rule: IsInvalid(score.UpdatedDate), Parameter: nameof(Score.UpdatedDate))
+                (Rule: IsInvalid(score.UpdatedDate), Parameter: nameof(Score.UpdatedDate)),
+                (Rule: IsNotRecent(score.CreatedDate), Parameter: nameof(Score.CreatedDate)),
+
+                (Rule: IsNotSame(
+                    firstDate: score.CreatedDate,
+                    secondDate: score.UpdatedDate,
+                    secondDateName: nameof(Score.UpdatedDate)),
+                 Parameter: nameof(Score.CreatedDate))
                 );
         }
 
@@ -48,6 +55,29 @@ namespace Tarteeb.Api.Services.Foundations.Scores
             Condition = date == default,
             Message = "Value is required"
         };
+
+        private dynamic IsNotRecent(DateTimeOffset date) => new
+        {
+            Condition = IsDateNotRecent(date),
+            Message = "Date is not recent."
+        };
+
+        private bool IsDateNotRecent(DateTimeOffset date)
+        {
+            DateTimeOffset currentDateTime = this.dateTimeBroker.GetCurrentDateTime();
+            TimeSpan timeDifference = currentDateTime.Subtract(date);
+
+            return timeDifference.TotalSeconds is > 60 or < 0;
+        }
+
+        private static dynamic IsNotSame(
+            DateTimeOffset firstDate,
+            DateTimeOffset secondDate,
+            string secondDateName) => new
+            {
+                Condition = firstDate != secondDate,
+                Message = $"Date is not same as {secondDateName}."
+            };
 
         private static void ValidateStorageScoreExists(Score maybeScore, Guid scoreId)
         {

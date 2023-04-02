153dffba23c07fd9ffcc1b108f84adeeae04794c
Nodirkhan Abdumurotov <nodirxonabdumurotov@mail.ru>
2023-04-02 15:17:32 +0500

    ShouldThrowValidationExceptionOnAddIfScoreIsInvalidAndLogItAsync -> FAIL

--- Tarteeb.Api.Tests.Unit/Services/Foundations/Scores/ScoreServiceTests.Validations.Add.cs
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Scores/ScoreServiceTests.Validations.Add.cs
@@ -44,5 +44,59 @@ namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Scores
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfScoreIsInvalidAndLogItAsync(
+          string invalidString)
+        {
+            // given
+            var invalidScore = new Score
+            {
+                EffortLink = invalidString
+            };
+
+            var invalidScoreException = new InvalidScoreException();
+
+            invalidScoreException.AddData(
+                key: nameof(Score.Id),
+                values: "Id is required");
+
+            invalidScoreException.AddData(
+                key: nameof(Score.EffortLink),
+                    values: "Text is required");
+
+            invalidScoreException.AddData(
+                key: nameof(Score.CreatedDate),
+                values: "Value is required");
+
+            invalidScoreException.AddData(
+                key: nameof(Score.UpdatedDate),
+                values: "Value is required");
+
+            var expectedScoreValidationException =
+                new ScoreValidationException(invalidScoreException);
+
+            // when
+            ValueTask<Score> addScoreTask = this.scoreService.AddScoreAsync(invalidScore);
+
+            ScoreValidationException actualScoreValidationException =
+                await Assert.ThrowsAsync<ScoreValidationException>(addScoreTask.AsTask);
+
+            // then
+            actualScoreValidationException.Should().BeEquivalentTo(expectedScoreValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedScoreValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertScoreAsync(It.IsAny<Score>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }

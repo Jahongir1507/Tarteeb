0f4c66679299f35feb1136bc817ceeca1069c4d5
ElbekDeveloper <e.normurodov@mail.ru>
2023-04-02 15:22:53 +0500

    ShouldSendEmailAsync -> FAIL

---
+++ Tarteeb.Api/Services/Foundations/Emails/EmailService.cs
@@ -0,0 +1,29 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System.Threading.Tasks;
+using Tarteeb.Api.Brokers.Emails;
+using Tarteeb.Api.Brokers.Loggings;
+using Tarteeb.Api.Models.Foundations.Emails;
+
+namespace Tarteeb.Api.Services.Foundations.Emails
+{
+    public partial class EmailService : IEmailService
+    {
+        private readonly IEmailBroker emailBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public EmailService(
+            IEmailBroker emailBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.emailBroker = emailBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<Email> SendEmailAsync(Email email) =>
+            throw new System.NotImplementedException();
+    }
+}
---
+++ Tarteeb.Api/Services/Foundations/Emails/IEmailService.cs
@@ -0,0 +1,15 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System.Threading.Tasks;
+using Tarteeb.Api.Models.Foundations.Emails;
+
+namespace Tarteeb.Api.Services.Foundations.Emails
+{
+    public interface IEmailService
+    {
+        ValueTask<Email> SendEmailAsync(Email email);
+    }
+}

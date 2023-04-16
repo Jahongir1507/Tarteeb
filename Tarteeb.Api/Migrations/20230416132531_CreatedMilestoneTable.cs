e44a6af0d72d9a895db78a44a286b20a88175d24
jahongir1507 <ishankulovjahongir1507@gmail.com>
2023-04-16 18:28:10 +0500

    DATA: Model & Migrate `Milestone`

---
+++ Tarteeb.Api/Migrations/20230416132531_CreatedMilestoneTable.cs
@@ -0,0 +1,44 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tarteeb.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatedMilestoneTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Milestones",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Discription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Deadline = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    AssigneeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    UpdatedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Milestones", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Milestones");
+        }
+    }
+}
---
+++ Tarteeb.Api/Models/Foundations/Milestones/Milestone.cs
@@ -0,0 +1,20 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System;
+
+namespace Tarteeb.Api.Models.Foundations.Milestones
+{
+    public class Milestone
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Discription { get; set; }
+        public DateTimeOffset Deadline { get; set; }
+        public Guid AssigneeId { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+    }
+}

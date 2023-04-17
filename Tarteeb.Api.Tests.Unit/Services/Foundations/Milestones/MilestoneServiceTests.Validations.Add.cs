d9ad59dfff3cee2e94e5d791a38254b0422dd3e8
ShohruhUzDev <shohruh7464784@mail.ru>
2023-04-17 22:11:18 +0500

    ShouldThrowValidationExceptionOnAddIfMilestoneIsInvalidAndLogItAsync ->FAIL

--- Tarteeb.Api.Tests.Unit/Services/Foundations/Milestones/MilestoneServiceTests.Validations.Add.cs
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Milestones/MilestoneServiceTests.Validations.Add.cs
@@ -45,5 +45,83 @@ namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Milestones
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfMilestoneIsInvalidAndLogItAsync(
+        string invalidText)
+        {
+            // given
+            var invalidMilestone = new Milestone
+            {
+                Title = invalidText
+            };
+
+            var invalidMilestoneException =
+                new InvalidMilestoneException();
+
+            invalidMilestoneException.AddData(
+                key: nameof(Milestone.Id),
+                values: "Id is required");
+
+            invalidMilestoneException.AddData(
+                key: nameof(Milestone.Title),
+                values: "Text is required");
+
+            invalidMilestoneException.AddData(
+                key: nameof(Milestone.Discription),
+                values: "Text is required");
+
+            invalidMilestoneException.AddData(
+                key: nameof(Milestone.Deadline),
+                values: "Date is required");
+
+            invalidMilestoneException.AddData(
+                key: nameof(Milestone.CreatedDate),
+                values: "Date is required");
+
+            invalidMilestoneException.AddData(
+                key: nameof(Milestone.UpdatedDate),
+                values: "Date is required");
+
+            invalidMilestoneException.AddData(
+                key: nameof(Milestone.AssigneeId),
+                values: "Id is required");
+
+            var expectedMilestoneValidationException =
+                new MilestoneValidationException(invalidMilestoneException);
+
+            // when
+            ValueTask<Milestone> addMilestoneTask =
+                this.milestoneService.AddMilestoneAsync(invalidMilestone);
+
+            MilestoneValidationException actualMilestoneValidationException =
+               await Assert.ThrowsAsync<MilestoneValidationException>(
+                   addMilestoneTask.AsTask);
+
+            // then
+            actualMilestoneValidationException.Should().BeEquivalentTo(
+                expectedMilestoneValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedMilestoneValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertMilestoneAsync(It.IsAny<Milestone>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
     }
 }

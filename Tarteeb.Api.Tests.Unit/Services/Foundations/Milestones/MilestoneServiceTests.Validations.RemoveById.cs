a99671fe34aaafb7d0f40e9c7ba23c53e66001dc
jahongir1507 <ishankulovjahongir1507@gmail.com>
2023-04-21 22:45:27 +0500

    ShouldThrowValidationExceptionOnRemoveIfIdIsInvalidAndLogItAsync -> FAIL

---
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Milestones/MilestoneServiceTests.Validations.RemoveById.cs
@@ -0,0 +1,56 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Tarteeb.Api.Models.Foundations.Milestones;
+using Tarteeb.Api.Models.Foundations.Milestones.Exceptions;
+using Xunit;
+
+namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Milestones
+{
+    public partial class MilestoneServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRemoveIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid invalidMilestoneId = Guid.Empty;
+            var invalidMilestoneException = new InvalidMilestoneException();
+
+            invalidMilestoneException.AddData(
+                key: nameof(Milestone.Id),
+                values: "Id is required");
+
+            var expectedMilestoneValidationException = new MilestoneValidationException(
+                invalidMilestoneException);
+
+            // when
+            ValueTask<Milestone> removeMilestoneByIdTask =
+                this.milestoneService.RemoveMilestoneByIdAsync(invalidMilestoneId);
+                
+            MilestoneValidationException actualMilestoneValidationException =
+                await Assert.ThrowsAsync<MilestoneValidationException>(
+                    removeMilestoneByIdTask.AsTask);
+
+            // then
+            actualMilestoneValidationException.Should().BeEquivalentTo(
+                expectedMilestoneValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedMilestoneValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteMilestoneAsync(It.IsAny<Milestone>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

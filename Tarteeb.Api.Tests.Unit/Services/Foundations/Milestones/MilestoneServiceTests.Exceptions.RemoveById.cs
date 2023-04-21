f1c65ee664356aaca66d69bc9078d06a51a07bf1
jahongir1507 <ishankulovjahongir1507@gmail.com>
2023-04-21 23:40:13 +0500

    ShouldThrowDependencyValidationExceptionOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync -> FAIL

---
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Milestones/MilestoneServiceTests.Exceptions.RemoveById.cs
@@ -0,0 +1,59 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Tarteeb.Api.Models.Foundations.Milestones;
+using Tarteeb.Api.Models.Foundations.Milestones.Exceptions;
+using Xunit;
+
+namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Milestones
+{
+    public partial class MilestoneServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyValidationExceptionOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someMilestoneId = Guid.NewGuid();
+            var dbUpdateConcurrencyException = new DbUpdateConcurrencyException();
+
+            var lockedMilestoneException =
+                new LockedMilestoneException(dbUpdateConcurrencyException);
+
+            var expectedMilestoneDependencyValidationException =
+                new MilestoneDependencyValidationException(lockedMilestoneException);
+
+            // when
+            ValueTask<Milestone> removeMilestoneByIdTask =
+                this.milestoneService.RemoveMilestoneByIdAsync(someMilestoneId);
+
+            MilestoneDependencyValidationException actualMilestoneDependencyValidationException =
+                await Assert.ThrowsAsync<MilestoneDependencyValidationException>(
+                    removeMilestoneByIdTask.AsTask);
+
+            // then
+            actualMilestoneDependencyValidationException.Should().BeEquivalentTo(
+                expectedMilestoneDependencyValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectMilestoneByIdAsync(It.IsAny<Guid>()), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedMilestoneDependencyValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteMilestoneAsync(It.IsAny<Milestone>()), Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

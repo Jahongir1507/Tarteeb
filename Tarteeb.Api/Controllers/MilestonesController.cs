521d19bc635fe6c8c457b61490858522d672dd1e
Jahongir Ishankulov <ishankulovjahongir1507@gmail.com>
2023-04-24 00:17:50 +0500

    CONTROLLERS: Post `Milestone`

---
+++ Tarteeb.Api/Controllers/MilestonesController.cs
@@ -0,0 +1,54 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using Tarteeb.Api.Models.Foundations.Milestones;
+using Tarteeb.Api.Models.Foundations.Milestones.Exceptions;
+using Tarteeb.Api.Services.Foundations.Milestones;
+
+namespace Tarteeb.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MilestonesController : RESTFulController
+    {
+        private readonly IMilestoneService milestoneServices;
+
+        public MilestonesController(IMilestoneService milestoneService) =>
+            this.milestoneServices = milestoneService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<Milestone>> PostMilestoneAsync(Milestone milestone)
+        {
+            try
+            {
+                return await this.milestoneServices.AddMilestoneAsync(milestone);
+            }
+            catch (MilestoneValidationException milestoneValidationException)
+            {
+                return BadRequest(milestoneValidationException.InnerException);
+            }
+            catch (MilestoneDependencyValidationException milestoneDependencyValidationException)
+                when (milestoneDependencyValidationException.InnerException is AlreadyExistsMilestoneException)
+            {
+                return Conflict(milestoneDependencyValidationException.InnerException);
+            }
+            catch (MilestoneDependencyValidationException milestoneDependencyValidationException)
+            {
+                return BadRequest(milestoneDependencyValidationException.InnerException);
+            }
+            catch (MilestoneDependencyException milestoneDependencyException)
+            {
+                return InternalServerError(milestoneDependencyException.InnerException);
+            }
+            catch (MilestoneServiceException milestoneServiceException)
+            {
+                return InternalServerError(milestoneServiceException.InnerException);
+            }
+        }
+    }
+}
